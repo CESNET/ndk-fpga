f94f0e668e8c76bd8d4e0e0665eb0b86d069d9d2
Jakub Cabal <cabal@cesnet.cz>
2022-05-03 14:54:49 +0200

    CARDS: [FEATURE] Add PCIE_GEN parameter

--- build/agi-hf400g/user_const.tcl
+++ build/agi-hf400g/user_const.tcl
@@ -4,8 +4,33 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+# ==============================================================================
+# PCIe parameters (not all combinations work):
+# ==============================================================================
+# Supported combinations for this card:
+# 1x PCIe Gen5 x8x8 -- PCIE_GEN=5, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1
+# 2x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=4, PCIE_ENDPOINT_MODE=1 (Note: default configuration)
+# 1x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1 (Note: limited DMA performance)
+# ------------------------------------------------------------------------------
+# PCIe Generation (possible values: 3, 4, 5):
+# 3 = PCIe Gen3
+# 4 = PCIe Gen4 (Stratix 10 with P-Tile or Agilex)
+# 5 = PCIe Gen5 (Agilex with R-Tile)
+set PCIE_GEN           4
+# PCIe endpoints (possible values: 1, 2, 4):
+# 1 = 1x PCIe x16 in one slot
+# 2 = 2x PCIe x16 in two slot OR 2x PCIe x8 in one slot (bifurcation x8+x8)
+# 4 = 4x PCIe x8 in two slots (bifurcation x8+x8)
+set PCIE_ENDPOINTS     4
+# PCIe endpoint mode (possible values: 0, 1):
+# 0 = 1x16 lanes
+# 1 = 2x8 lanes (bifurcation x8+x8)
+set PCIE_ENDPOINT_MODE 1
+# ------------------------------------------------------------------------------
+
+# ==============================================================================
 # DMA parameters:
-# ===============
+# ==============================================================================
 # If you do not have access to a non-public repository with DMA IP, set to false.
 # If the DMA module is disabled, loopback will be implemented instead.
 set DMA_ENABLE           true
@@ -15,6 +40,7 @@ set DMA_TX_CHANNELS      32
 # In blocking mode, packets are dropped only when the RX DMA channel is off.
 # In non-blocking mode, packets are dropped whenever they cannot be sent.
 set DMA_RX_BLOCKING_MODE true
+# ------------------------------------------------------------------------------
 
 # Other parameters:
 # =================
--- build/dk-dev-1sdx-p/user_const.tcl
+++ build/dk-dev-1sdx-p/user_const.tcl
@@ -4,20 +4,46 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+# ==============================================================================
+# PCIe parameters (not all combinations work):
+# ==============================================================================
+# Supported combinations for this card:
+# 1x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1 (Note: default configuration)
+# 1x PCIe Gen4 x16  -- PCIE_GEN=4, PCIE_ENDPOINTS=1, PCIE_ENDPOINT_MODE=0 (Note: worse DMA performance)
+# 2x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=4, PCIE_ENDPOINT_MODE=1 (Note: only for DMA_400G_DEMO)
+# ------------------------------------------------------------------------------
+# PCIe Generation (possible values: 3, 4, 5):
+# 3 = PCIe Gen3
+# 4 = PCIe Gen4 (Stratix 10 with P-Tile or Agilex)
+# 5 = PCIe Gen5 (Agilex with R-Tile)
+set PCIE_GEN           4
+# PCIe endpoints (possible values: 1, 2, 4):
+# 1 = 1x PCIe x16 in one slot
+# 2 = 2x PCIe x16 in two slot OR 2x PCIe x8 in one slot (bifurcation x8+x8)
+# 4 = 4x PCIe x8 in two slots (bifurcation x8+x8)
+set PCIE_ENDPOINTS     2
+# PCIe endpoint mode (possible values: 0, 1):
+# 0 = 1x16 lanes
+# 1 = 2x8 lanes (bifurcation x8+x8)
+set PCIE_ENDPOINT_MODE 1
+# ------------------------------------------------------------------------------
+
+# ==============================================================================
 # DMA parameters:
-# ===============
+# ==============================================================================
 # If you do not have access to a non-public repository with DMA IP, set to false.
 # If the DMA module is disabled, loopback will be implemented instead.
 set DMA_ENABLE           true
+# The minimum number of RX/TX DMA channels for this card is 16.
 set DMA_RX_CHANNELS      16
 set DMA_TX_CHANNELS      16
 # In blocking mode, packets are dropped only when the RX DMA channel is off.
 # In non-blocking mode, packets are dropped whenever they cannot be sent.
 set DMA_RX_BLOCKING_MODE true
-# Special example of 400G DMA, Ethernet is not connected to DMA.
+# Special example of 400G DMA, Ethernet is not connected to DMA and must be set 
+# special PCIe config.: 2x PCIe Gen4 x8x8, requires PCIe expansion connector.
 set DMA_400G_DEMO        false
-# if DMA_400G_DEMO=True then PCIE_ENDPOINTS must be 4!
-set PCIE_ENDPOINTS       2
+# ------------------------------------------------------------------------------
 
 # Other parameters:
 # =================
