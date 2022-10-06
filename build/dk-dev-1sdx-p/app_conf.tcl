99f108712d452c77aa48f692bfe1a3538bd7b945
Vladislav Valek <vladislav.valek@email.cz>
2022-10-06 10:41:34 +0200

    user_const -> app_conf [MAINTENANCE]: removed unused parameters and parameter values
    Also changed delimiter literal. The parameters were pruned because not
    every card supports the parameters combinations which it has written in
    its app_conf file.

--- build/dk-dev-1sdx-p/app_conf.tcl
+++ build/dk-dev-1sdx-p/app_conf.tcl
@@ -8,18 +8,15 @@
 # Parametrizing section of the NDK-CORE documentation.
 
 # ------------------------------------------------------------------------------
->>>>>>> 53154d7 (dk-dev-1sdx-p/app_conf [MAINTENANCE]: added reference to docs):build/dk-dev-1sdx-p/app_conf.tcl
 # PCIe parameters (not all combinations work):
-# ==============================================================================
+# ------------------------------------------------------------------------------
 # Supported combinations for this card:
 # 1x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1 (Note: default configuration)
 # 1x PCIe Gen4 x16  -- PCIE_GEN=4, PCIE_ENDPOINTS=1, PCIE_ENDPOINT_MODE=0 (Note: worse DMA performance)
 # 2x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=4, PCIE_ENDPOINT_MODE=1 (Note: only for DMA_400G_DEMO)
 # ------------------------------------------------------------------------------
-# PCIe Generation (possible values: 3, 4, 5):
-# 3 = PCIe Gen3
+# PCIe Generation (possible values: 4):
 # 4 = PCIe Gen4 (Stratix 10 with P-Tile or Agilex)
-# 5 = PCIe Gen5 (Agilex with R-Tile)
 set PCIE_GEN           4
 # PCIe endpoints (possible values: 1, 2, 4):
 # 1 = 1x PCIe x16 in one slot
@@ -30,14 +27,10 @@ set PCIE_ENDPOINTS     2
 # 0 = 1x16 lanes
 # 1 = 2x8 lanes (bifurcation x8+x8)
 set PCIE_ENDPOINT_MODE 1
-# ------------------------------------------------------------------------------
 
-# ==============================================================================
+# ------------------------------------------------------------------------------
 # DMA parameters:
-# ==============================================================================
-# If you do not have access to a non-public repository with DMA IP, set to false.
-# If the DMA module is disabled, loopback will be implemented instead.
-set DMA_ENABLE           true
+# ------------------------------------------------------------------------------
 # The minimum number of RX/TX DMA channels for this card is 16.
 set DMA_RX_CHANNELS      16
 set DMA_TX_CHANNELS      16
@@ -47,12 +40,10 @@ set DMA_RX_BLOCKING_MODE true
 # Special example of 400G DMA, Ethernet is not connected to DMA and must be set 
 # special PCIe config.: 2x PCIe Gen4 x8x8, requires PCIe expansion connector.
 set DMA_400G_DEMO        false
-# ------------------------------------------------------------------------------
 
-# ==============================================================================
+# ------------------------------------------------------------------------------
 # Other parameters:
-# ==============================================================================
+# ------------------------------------------------------------------------------
 set PROJECT_NAME "NDK_MINIMAL"
 set PROJECT_VARIANT "$ETH_PORT_SPEED(0)G$ETH_PORTS"
 set PROJECT_VERSION [exec cat ../../VERSION]
-# ------------------------------------------------------------------------------
--- build/fb2cghh/app_conf.tcl
+++ build/fb2cghh/app_conf.tcl
@@ -1,6 +1,7 @@
-# user_const.tcl: User parameters for fb2cghh card
+# app_conf.tcl: Application specific parameters which can be changed by the user
 # Copyright (C) 2022 CESNET z.s.p.o.
 # Author(s): David Beneš <benes.david2000@seznam.cz>
+#            Vladislav Valek <valekv@cesnet.cz>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,22 +10,18 @@
 
 # ------------------------------------------------------------------------------
 # DMA parameters:
-# ==============================================================================
-# If you do not have access to a non-public repository with DMA IP, set to false.
-# If the DMA module is disabled, loopback will be implemented instead.
-set DMA_ENABLE           true
+# ------------------------------------------------------------------------------
 # The minimum number of RX/TX DMA channels for this card is 16.
 set DMA_RX_CHANNELS      16
 set DMA_TX_CHANNELS      16
 # In blocking mode, packets are dropped only when the RX DMA channel is off.
 # In non-blocking mode, packets are dropped whenever they cannot be sent.
 set DMA_RX_BLOCKING_MODE true
-# ------------------------------------------------------------------------------
 
-# ==============================================================================
+
+# ------------------------------------------------------------------------------
 # Other parameters:
-# ==============================================================================
+# ------------------------------------------------------------------------------
 set PROJECT_NAME "NDK_MINIMAL"
 set PROJECT_VARIANT "$ETH_PORT_SPEED(0)G$ETH_PORTS"
 set PROJECT_VERSION [exec cat ../../VERSION]
-# ------------------------------------------------------------------------------
