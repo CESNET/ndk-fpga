53423e20671c3a3b94c04a3d261e59a85994712b
Jakub Cabal <cabal@cesnet.cz>
2022-10-07 17:35:05 +0200

    APP: [MAINTENANCE] Update APP-CORE DeviceTree

--- app/intel/DevTree.tcl
+++ app/intel/DevTree.tcl
@@ -10,10 +10,12 @@ proc dts_application {base eth_streams mem_ports} {
     set subaddr_w [expr 0x02000000 / $mi_ports]
 
 	set ret ""
-	append ret "nic_application {"
-	append ret "reg = <$base [expr $subaddr_w * $eth_streams]>;"
-    # TODO: why it was commented?
-    append ret "compatible = \"netcope,application,nic\";"
+	append ret "application {"
+
+    for {set i 0} {$i < $eth_streams} {incr i} {
+            set core_base [expr $base + $subaddr_w*$i]
+            append ret [dts_app_minimal_core $i $core_base $subaddr_w]
+    }
 
     for {set i 0} {$i < $mem_ports} {incr i} {
             set mem_tester_base [expr $base + $subaddr_w * ($eth_streams + $i)]
@@ -21,6 +23,15 @@ proc dts_application {base eth_streams mem_ports} {
     }
 
 	append ret "};"
+	return $ret
+}
+
+proc dts_app_minimal_core {index base reg_size} {
+	set ret ""
+	append ret "app_core_minimal_$index {"
+	append ret "reg = <$base $reg_size>;"
+    append ret "compatible = \"cesnet,minimal,app_core\";"
+    append ret "};"
 	return $ret
 }
 
