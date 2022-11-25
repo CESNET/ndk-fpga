b2de3c6090e86affb3ff497f6f79c8e6c982cc41
Jakub Cabal <cabal@cesnet.cz>
2022-11-25 14:50:22 +0100

    NDK: [BUGFIX] Add missing SPDX-License-Identifier

--- build/nfb-200g2ql/app_conf.tcl
+++ build/nfb-200g2ql/app_conf.tcl
@@ -2,6 +2,7 @@
 # Copyright (C) 2022 CESNET z.s.p.o.
 # Author(s): Daniel Kriz <danielkriz@cesnet.cz>
 #
+# SPDX-License-Identifier: BSD-3-Clause
 
 # NOTE: The detailed description of the usage of this file can be viewed in the
 # Parametrizing section of the NDK-CORE documentation.
