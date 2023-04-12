2756e4cedc545b1cc1a6420d6d82850836994203
Vladislav Valek <vladislav.valek@email.cz>
2023-04-12 08:52:42 +0200

    build/vcu118 [FEATURE]: add new card build files

---
+++ build/vcu118/app_conf.tcl
@@ -0,0 +1,25 @@
+# app_conf.tcl: User parameters for fb4cgg3/fb2cgg3 card
+# Copyright (C) 2023 CESNET z.s.p.o.
+# Author(s): Vladislav Valek <valekv@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# NOTE: The detailed description of the usage of this file can be viewed in the
+# Parametrizing section of the NDK-CORE documentation.
+
+# ------------------------------------------------------------------------------
+# DMA parameters:
+# ------------------------------------------------------------------------------
+# The minimum number of RX/TX DMA channels for this card is 16.
+set DMA_RX_CHANNELS      16
+set DMA_TX_CHANNELS      16
+# In blocking mode, packets are dropped only when the RX DMA channel is off.
+# In non-blocking mode, packets are dropped whenever they cannot be sent.
+set DMA_RX_BLOCKING_MODE true
+
+# ------------------------------------------------------------------------------
+# Other parameters:
+# ------------------------------------------------------------------------------
+set PROJECT_NAME "NDK_MINIMAL"
+set PROJECT_VARIANT "$ETH_PORT_SPEED(0)G$ETH_PORTS"
+set PROJECT_VERSION [exec cat ../../VERSION]
