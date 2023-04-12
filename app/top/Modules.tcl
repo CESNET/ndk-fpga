c8dda9d08aa112f2f670127211b9f82c5802e220
Vladislav Valek <vladislav.valek@email.cz>
2023-04-12 08:51:44 +0200

    [MAINTENANCE]: add option to disable application core
    The component is then replaced by the empty architecture which contains
    loopbacks for either DMA or NETWORK_MOD if those are enabled.

--- app/top/Modules.tcl
+++ app/top/Modules.tcl
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # converting input list to associative array (uncomment when needed)
-# array set ARCHGRP_ARR $ARCHGRP
+array set ARCHGRP_ARR $ARCHGRP
 
 # Component paths
 set MI_ASYNC_BASE       "$OFM_PATH/comp/mi_tools/async"
@@ -25,19 +25,24 @@ lappend PACKAGES "$OFM_PATH/comp/base/pkg/math_pack.vhd"
 lappend PACKAGES "$OFM_PATH/comp/base/pkg/type_pack.vhd"
 lappend PACKAGES "$OFM_PATH/comp/base/pkg/eth_hdr_pack.vhd"
 
-# Components
-lappend COMPONENTS [ list "MI_ASYNC"       $MI_ASYNC_BASE       "FULL" ]
-lappend COMPONENTS [ list "MI_SPLITTER"    $MI_SPLITTER_BASE    "FULL" ]
-lappend COMPONENTS [ list "MFB_META_INS"   $MFB_META_INS_BASE   "FULL" ]
-lappend COMPONENTS [ list "MFB_PIPE"       $MFB_PIPE_BASE       "FULL" ]
-lappend COMPONENTS [ list "MVB_PIPE"       $MVB_PIPE_BASE       "FULL" ]
-lappend COMPONENTS [ list "STREAMS_MERGER" $STREAMS_MERGER_BASE "FULL" ]
-lappend COMPONENTS [ list "DMA_CHAN_MOD"   $DMA_CHAN_MOD_BASE   "FULL" ]
-lappend COMPONENTS [ list "MVB_CHDIST"     $MVB_CHDIST_BASE     "FULL" ]
-lappend COMPONENTS [ list "MEM_TESTER"     $MEM_TESTER_BASE     "FULL" ]
-lappend COMPONENTS [ list "MEM_LOGGER"     $MEM_LOGGER_BASE     "FULL" ]
+if {$ARCHGRP_ARR(APP_CORE_ENABLE)} {
+    # Components
+    lappend COMPONENTS [ list "MI_ASYNC"       $MI_ASYNC_BASE       "FULL" ]
+    lappend COMPONENTS [ list "MI_SPLITTER"    $MI_SPLITTER_BASE    "FULL" ]
+    lappend COMPONENTS [ list "MFB_META_INS"   $MFB_META_INS_BASE   "FULL" ]
+    lappend COMPONENTS [ list "MFB_PIPE"       $MFB_PIPE_BASE       "FULL" ]
+    lappend COMPONENTS [ list "MVB_PIPE"       $MVB_PIPE_BASE       "FULL" ]
+    lappend COMPONENTS [ list "STREAMS_MERGER" $STREAMS_MERGER_BASE "FULL" ]
+    lappend COMPONENTS [ list "DMA_CHAN_MOD"   $DMA_CHAN_MOD_BASE   "FULL" ]
+    lappend COMPONENTS [ list "MVB_CHDIST"     $MVB_CHDIST_BASE     "FULL" ]
+    lappend COMPONENTS [ list "MEM_TESTER"     $MEM_TESTER_BASE     "FULL" ]
+    lappend COMPONENTS [ list "MEM_LOGGER"     $MEM_LOGGER_BASE     "FULL" ]
 
-# Files
-lappend MOD "$ENTITY_BASE/app_subcore.vhd"
-lappend MOD "$ENTITY_BASE/application_core.vhd"
+    # Files
+    lappend MOD "$ENTITY_BASE/app_subcore.vhd"
+    lappend MOD "$ENTITY_BASE/application_core.vhd"
+} else {
+    lappend MOD "$APP_CORE_UTILS_BASE/app_core_empty_arch.vhd"
+}
+
 lappend MOD "$ENTITY_BASE/DevTree.tcl"
