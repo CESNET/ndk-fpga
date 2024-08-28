76aec04c13282f8a7eca5cb1100bcf3aa428a554
Radek Isa <isa@cesnet.cz>
2024-08-28 10:25:02 +0200

    APP UVM: [MAITENANCE] Change input to model from MFB+MVB to packet.     Unite MFB+MVB

--- app/uvm/env/model.sv
+++ app/uvm/env/model.sv
@@ -58,11 +58,8 @@ class model #(ETH_STREAMS, ETH_CHANNELS, ETH_RX_HDR_WIDTH, DMA_STREAMS, DMA_RX_C
     task run_eth(uvm_phase phase, int unsigned index);
         uvm_app_core_top_agent::sequence_eth_item#(2**8, 16, ITEM_WIDTH) item;
         uvm_app_core::packet #(DMA_HDR_META_WIDTH, DMA_RX_CHANNELS, DMA_PKT_MTU, ITEM_WIDTH) packet;
-        //uvm_logic_vector_array::sequence_item#(ITEM_WIDTH) packet_mfb;
-        //uvm_app_core::packet_header #(DMA_HDR_META_WIDTH, DMA_RX_CHANNELS, DMA_PKT_MTU) packet_hdr;
 
         forever begin
-            
             //get item
             eth_rx[index].get(item);
 
@@ -83,28 +80,6 @@ class model #(ETH_STREAMS, ETH_CHANNELS, ETH_RX_HDR_WIDTH, DMA_STREAMS, DMA_RX_C
             end else begin
                 dma_tx[index].write(packet);
             end
-           //packet_mfb = uvm_logic_vector_array::sequence_item#(ITEM_WIDTH)::type_id::create("packet_mfb", this);
-            //packet_mfb.start = item.start;
-            //packet_hdr = uvm_app_core::packet_header #(DMA_HDR_META_WIDTH, DMA_RX_CHANNELS, DMA_PKT_MTU)::type_id::create("packet_hdr", this);
-            //packet_hdr.start = item.start;
-
-            //packet_mfb.data = item.data; 
-            //packet_hdr.meta = '0;
-            //if (DMA_STREAMS != ETH_STREAMS) begin
-            //    packet_hdr.channel = (index*APP_RX_CHANNELS) + eth_to_dma[index].port_get(item.channel%ETH_CHANNELS);
-            //end else begin
-            //    packet_hdr.channel = eth_to_dma[index].port_get(item.channel%ETH_CHANNELS);
-            //end
-            //packet_hdr.packet_size = item.data.size();
-            //packet_hdr.discard     = 0;
-
-            //if (DMA_STREAMS == 1) begin
-            //    dma_mvb_tx[0].write(packet_hdr);
-            //    dma_mfb_tx[0].write(packet_mfb);
-            //end else begin
-            //    dma_mvb_tx[index].write(packet_hdr);
-            //    dma_mfb_tx[index].write(packet_mfb);
-            //end
         end
     endtask
 
@@ -129,36 +104,8 @@ class model #(ETH_STREAMS, ETH_CHANNELS, ETH_RX_HDR_WIDTH, DMA_STREAMS, DMA_RX_C
 
             eth_tx[eth_channel/ETH_CHANNELS].write(packet);
 
-            //packet_hdr  = uvm_app_core::packet_header #(0, 2**ETH_TX_CHANNEL_WIDTH, 2**ETH_TX_LENGTH_WIDTH-1)::type_id::create("packet_hdr", this);
-            //packet_hdr.start = item.start;
-            //packet_data = uvm_logic_vector_array::sequence_item#(ITEM_WIDTH)::type_id::create("packet_data", this);
-            //packet_data.start = item.start;
-
-            //eth_channel = ((index * DMA_TX_CHANNELS) + item.channel)/((DMA_STREAMS*DMA_TX_CHANNELS)/(ETH_STREAMS*ETH_CHANNELS));
-            //packet_hdr.packet_size = item.data.size();
-            //packet_hdr.channel = eth_channel; 
-            //packet_hdr.discard = 1'b0;
-
-            //packet_data.data  = item.data;
-
-            //eth_mvb_tx[eth_channel/ETH_CHANNELS].write(packet_hdr);
-            //eth_mfb_tx[eth_channel/ETH_CHANNELS].write(packet_data);
         end
     endtask
-
-    //task run_eth(uvm_phase phase, int unsigned index);
-    //    fork
-    //       run_eth_mfb(index);
-    //       run_eth_mvb(index);
-    //    join
-    //endtask
-
-    //task run_dma(uvm_phase phase, int unsigned index);
-    //     fork
-    //       run_dma_mfb(index);
-    //       run_dma_mvb(index);
-    //    join
-    //endtask
 
     function void report_phase(uvm_phase phase);
     endfunction
