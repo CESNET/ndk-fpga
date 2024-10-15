7857e2924bba2ec909e91c648a8f42f3089d37d4
Martin Spinler <spinler@cesnet.cz>
2024-10-15 15:43:13 +0200

    [MAINTENANCE] code style - Python: ignore extra/ submodules

--- tests/ci/pycodestyle.sh
+++ tests/ci/pycodestyle.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-flake8 --extend-exclude "ver_settings.py,synth_settings.py" --statistics --extend-ignore=T201,T202,B902,F821,E501,E203,E221,E261,E265,E266
+flake8 --extend-exclude "ver_settings.py,synth_settings.py,extra/" --statistics --extend-ignore=T201,T202,B902,F821,E501,E203,E221,E261,E265,E266
