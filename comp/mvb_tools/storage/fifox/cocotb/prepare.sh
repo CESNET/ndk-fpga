b71f541accbd1ba89416d2bed803203adf175033
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-02-13 18:23:10 +0100

    fix(mvb_tools): update outdated prepare script of MVB FIFOX test

--- comp/mvb_tools/storage/fifox/cocotb/prepare.sh
+++ comp/mvb_tools/storage/fifox/cocotb/prepare.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (C) 2025 CESNET z. s. p. o.
+# Author(s): Ondřej Schwarz <ondrejschwarz@cesnet.cz>
 
-ROOT_PATH=../../../../..
+NDK_FPGA_PATH=../../../../..
+source $NDK_FPGA_PATH/env.sh
 
-PKG_COCOTBEXT_OFM=$ROOT_PATH/python/cocotbext/
+ndk_fpga_venv_prepare "venv-fifox"
 
-# Python virtual environment
-python -m venv venv-fifox
-source venv-fifox/bin/activate
-
-python -m pip install setuptools
-python -m pip install $PKG_COCOTBEXT_OFM
+pip install -e .
 
 echo ""
 echo "Now activate environment with:"
