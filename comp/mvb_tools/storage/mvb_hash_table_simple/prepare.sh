555c895804cdd61aa2a5aed2aa99fa5c1e405fd8
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-02-13 18:29:06 +0100

    fix(mvb_tools): update outdated prepare script in mvb hash table simple

--- comp/mvb_tools/storage/mvb_hash_table_simple/prepare.sh
+++ comp/mvb_tools/storage/mvb_hash_table_simple/prepare.sh
@@ -1,29 +1,16 @@
 #!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (C) 2025 CESNET z. s. p. o.
+# Author(s): Ondřej Schwarz <ondrejschwarz@cesnet.cz>
 
-ROOT_PATH=../../../../..
+NDK_FPGA_PATH=../../../..
+source $NDK_FPGA_PATH/env.sh
 
-#swbase=../../../swbase/
-swbase=git+https://github.com/CESNET/ndk-sw.git#subdirectory=
+ndk_fpga_venv_prepare "venv-mvb_hash_table_simple"
 
-PKG_PYNFB=${swbase}pynfb/
-PKG_LIBNFBEXT_PYTHON=${swbase}ext/libnfb_ext_python/
-PKG_COCOTBEXT=$ROOT_PATH/python/cocotbext/
-
-# Python virtual environment
-python -m venv venv-cocotb
-source venv-cocotb/bin/activate
-
-python -m pip install cython wheel setuptools
-python -m pip install pylibfdt fdt
-python -m pip install scapy
-python -m pip install colorama
-python -m pip install pyyaml
-python -m pip wheel -w ./cocotbwheels $PKG_PYNFB
-python -m pip install --find-links ./cocotbwheels nfb
-python -m pip install --find-links ./cocotbwheels $PKG_LIBNFBEXT_PYTHON
-python -m pip install $PKG_COCOTBEXT
+pip install -e .
 
 echo ""
 echo "Now activate environment with:"
-echo "source venv-cocotb/bin/activate"
+echo "source venv-mvb_hash_table_simple/bin/activate"
 
