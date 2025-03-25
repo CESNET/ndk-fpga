68d523ec4d9023318da46e95c5ee0f1bd915c1a5
Daniel Kondys <xkondy00@vutbr.cz>
2025-03-25 15:17:10 +0100

    feat(python-ofm): integrate Speed Meters to the GLS module

--- python/ofm/ofm/comp/mfb_tools/debug/gen_loop_switch/gen_loop_switch.py
+++ python/ofm/ofm/comp/mfb_tools/debug/gen_loop_switch/gen_loop_switch.py
@@ -3,39 +3,46 @@
 # Author(s): Daniel Kondys <kondys@cesnet.cz>
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple
 
 import nfb
-from ofm.comp.mfb_tools.debug.generator import MfbGenerator, GeneratorConfig
+from ofm.comp.mfb_tools.debug.generator import MfbGenerator
+from ofm.comp.mfb_tools.logic.speed_meter import SpeedMeter
 
 @dataclass
 class StreamNodes:
     """A helpful class to encapsulate information about the nodes of a GlsStream."""
-    generator_idx: int
+    tx_sm_name: str
+    rx_sm_name: str
+    gen_name: str
     rx_mux_offset: int
     tx_mux_offset: int
 
 class GlsStream:
     """Top (left2right) or bottom (right2left) part of the GenLoopSwitch.
 
-    Contains appropriate Generator object and address offsets of the Multiplexers.
+    Contains the appropriate Generator and Speed Meter objects and address offsets of the Multiplexers.
 
     Usage:
     Methods like `gen_start` and `gen_stop` that abstract or simplify the usage of the generator.
-    Other methods/properties are accessible though the generator object.
+    Other methods/properties are accessible though the generator and speed meter objects.
 
     Example:
     gls = GenLoopSwitch()
     gls.l2r.gen.frame_length = 512
     gls.l2r.gen.enable = True
+    dma_rx_speed = gls.l2r.tx_sm_measure()
     """
 
-    def __init__(self, dev, node, comp, stream_nodes):
+    def __init__(self, dev, node, comp, stream_nodes, sm_exist):
         self._comp = comp
-        self._gen_id = stream_nodes.generator_idx
         self._rx_mux = stream_nodes.rx_mux_offset
         self._tx_mux = stream_nodes.tx_mux_offset
-        self.gen = MfbGenerator(dev=dev, node=node.nodes[self._gen_id])
+
+        if sm_exist:
+            self.tx_sm = SpeedMeter(dev=dev, node=node.get_subnode(stream_nodes.tx_sm_name), lightweight=True)
+            self.rx_sm = SpeedMeter(dev=dev, node=node.get_subnode(stream_nodes.rx_sm_name), lightweight=True)
+        self.gen = MfbGenerator(dev=dev, node=node.get_subnode(stream_nodes.gen_name))
         # self.player = ...
 
     # ############
@@ -95,6 +102,17 @@ class GlsStream:
         else:
             self.mux_loopback = 0
             self.mux_generator = n
+
+    # ############
+    # Speed Meters
+    # ############
+    def tx_sm_measure(self, freq: int = 2*10**8) -> Tuple[float, float | None]:
+        """Measure throughput speed on the TX interface of this Stream, return [bps]."""
+        return GenLoopSwitch.sm_measure(self.tx_sm)
+
+    def rx_sm_measure(self, freq: int = 2*10**8) -> Tuple[float, float | None]:
+        """Measure throughput speed on the RX interface of this Stream, return [bps]."""
+        return GenLoopSwitch.sm_measure(self.rx_sm)
 
     # #############
     # MFB Generator
@@ -165,9 +183,9 @@ class GenLoopSwitch(nfb.BaseComp):
     Familiarity with the gen_loop_switch FW component (at least its documentation) is strongly
     advised.
 
-    The user can control the (generator and loopback) multiplexers, generators using one of its
-    "Stream" instances. The GLS is virtually split into two Streams: left2right and right2left.
-    Each of them manages what is forwarded to its output.
+    The user can control the (generator and loopback) multiplexers, generators, and speed meters
+    using one of its "Stream" instances. The GLS is virtually split into two Streams: left2right
+    and right2left. Each of them manages what is forwarded to its output.
 
     Example:
     The left2right Stream (l2r) selects the source of data forwarded out of the right side.
@@ -181,6 +199,9 @@ class GenLoopSwitch(nfb.BaseComp):
     To generate data to the right side using a MFB Generator, the simplest way is to use the
     gen_start method like so: `gls.l2r.gen_start(en_path=True)`.
     To get the full configuration of the (l2r) MFB Generator: `gls.l2r.gen.get_configuration()`.
+    To measure speed on the right side on the TX interface:
+     - option 1: `GenLoopSwitch.sm_measure(gls.l2r.tx_sm)`
+     - option 2: `gls.l2r.tx_sm_measure()`
     """
 
     DT_COMPATIBLE = "cesnet,ofm,gen_loop_switch"
@@ -193,8 +214,36 @@ class GenLoopSwitch(nfb.BaseComp):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        l2r_nodes = StreamNodes(0, self._REG_MUX_C, self._REG_MUX_A)
-        r2l_nodes = StreamNodes(1, self._REG_MUX_D, self._REG_MUX_B)
+        l2r_nodes = StreamNodes(
+            # The names are taken from the Device Tree (they must correspond).
+            tx_sm_name = "l2r_tx_speed_meter",
+            rx_sm_name = "l2r_rx_speed_meter",
+            gen_name = "mfb_gen2dma",
+            rx_mux_offset = self._REG_MUX_C,
+            tx_mux_offset = self._REG_MUX_A,
+        )
+        r2l_nodes = StreamNodes(
+            # The names are taken from the Device Tree (they must correspond).
+            tx_sm_name = "r2l_tx_speed_meter",
+            rx_sm_name = "r2l_rx_speed_meter",
+            gen_name = "mfb_gen2eth",
+            rx_mux_offset = self._REG_MUX_D,
+            tx_mux_offset = self._REG_MUX_B,
+        )
 
-        self.l2r = GlsStream(dev=self._dev, node=self._node, comp=self._comp, stream_nodes=l2r_nodes)
-        self.r2l = GlsStream(dev=self._dev, node=self._node, comp=self._comp, stream_nodes=r2l_nodes)
+        # Checking for the presence of SpeedMeters, may not be available in older versions of FW.
+        # There should always be all SpeedMeters or none.
+        self.speedmeters_exist = True
+        if not self._node.exist_subnode(l2r_nodes.tx_sm_name):
+            print(f"Warning: SpeedMeters of the {self._node.name} not found in the Device Tree.")
+            print(f"         Unable to use methods to measure speed.")
+            print(f"         Try using a newer FW.")
+            self.speedmeters_exist = False
+
+        self.l2r = GlsStream(self._dev, self._node, self._comp, l2r_nodes, self.speedmeters_exist)
+        self.r2l = GlsStream(self._dev, self._node, self._comp, r2l_nodes, self.speedmeters_exist)
+
+    @staticmethod
+    def sm_measure(sm: SpeedMeter, to: float = 0.1, freq: Optional[int] = 2*10**8) -> Tuple[float, float | None]:
+        """Use the given SpeedMeter to measure the throughput speed, return [bps]."""
+        return sm.measure(to, freq)
