8f5d63f4bb443b566a2361aa113e7d33a3974a12
Jakub Cabal <cabal@cesnet.cz>
2025-04-02 21:46:08 +0200

    fix(MAC-LITE): fix AVST adapter in rx_mac_lite_ill100ge wrapper

--- comp/nic/mac_lite/rx_mac_lite/top/ill100ge/rx_mac_lite_ill100ge.vhd
+++ comp/nic/mac_lite/rx_mac_lite/top/ill100ge/rx_mac_lite_ill100ge.vhd
@@ -43,7 +43,7 @@ entity RX_MAC_LITE_ILL100GE is
         -- Enable of MAC checking.
         MAC_CHECK_EN    : boolean := true;
         -- Number of maximum MAC address in CAM memory, maximum value is 16.
-        MAC_COUNT       : natural := 4;
+        MAC_COUNT       : natural := 16;
         -- Enable of timestamping frames.
         TIMESTAMP_EN    : boolean := true;
         -- Select correct FPGA device.
@@ -147,7 +147,7 @@ architecture FULL of RX_MAC_LITE_ILL100GE is
 begin
 
     -- avst simple module support only readyLatency=0
-    avst_simple_i : entity work.RX_MAC_LITE_ADAPTER_AVST_SIMPLE
+    avst_simple_i : entity work.ETH_AVST_ADAPTER
     generic map(
         DATA_WIDTH => REGIONS*REGION_SIZE*BLOCK_SIZE*ITEM_WIDTH
     )
