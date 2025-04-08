aeea382e9b783b74b758861500b89b2597aecfe4
Radek Isa <isa@cesnet.cz>
2025-04-08 12:38:08 +0200

    style(pcie-mod-uvm): remove unused verification environment from pcie mode verification

--- core/comp/pcie/pcie_mod/uvm/Modules.tcl
+++ core/comp/pcie/pcie_mod/uvm/Modules.tcl
@@ -19,8 +19,6 @@ lappend COMPONENTS [ list "SV_PROBE"                    "$OFM_PATH/comp/uvm/prob
 
 lappend MOD "$OFM_PATH/comp/base/pkg/dma_bus_pack.sv"
 lappend MOD "$OFM_PATH/comp/pcie/ptc/uvm/tbench/info/pkg.sv"
-#lappend MOD "$OFM_PATH/comp/pcie/ptc/uvm/tbench/info_rc/pkg.sv"
-#lappend MOD "$OFM_PATH/comp/pcie/ptc/uvm/tbench/pcie_rc/pkg.sv"
 lappend MOD "$OFM_PATH/comp/base/pkg/pcie_meta_pack.sv"
 
 lappend MOD "$ENTITY_BASE/tbench/pcie/pkg.sv"
