3bfbd0f38de3421ec81b5c0f05237621cbbd661e
Radek Isa <isa@cesnet.cz>
2025-04-08 14:35:12 +0200

    fix(uvm): change parent class sequence sequence_lib_pcie_rx
            change parent class from uvm_common::sequence_library to sequence_lib_rx

--- comp/uvm/logic_vector_array_mfb/sequence_lib.sv
+++ comp/uvm/logic_vector_array_mfb/sequence_lib.sv
@@ -45,3 +45,24 @@ class sequence_lib_rx_speed #(int unsigned REGIONS, int unsigned REGION_SIZE, in
     endfunction
 endclass
 
+
+class sequence_lib_pcie_rx #(int unsigned REGIONS, int unsigned REGION_SIZE, int unsigned BLOCK_SIZE, int unsigned ITEM_WIDTH, int unsigned META_WIDTH) extends sequence_lib_rx#(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH);
+  `uvm_object_param_utils(uvm_logic_vector_array_mfb::sequence_lib_pcie_rx#(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH))
+  `uvm_sequence_library_utils(uvm_logic_vector_array_mfb::sequence_lib_pcie_rx#(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH))
+
+  function new(string name = "sequence_lib_pcie_rx");
+    super.new(name);
+    init_sequence_library();
+  endfunction
+
+    // subclass can redefine and change run sequences
+    // can be useful in specific tests
+    virtual function void init_sequence(config_sequence param_cfg = null);
+        uvm_common::sequence_library::init_sequence(param_cfg);
+        this.add_sequence(uvm_logic_vector_array_mfb::sequence_full_speed_pcie_rx #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)::get_type());
+        this.add_sequence(uvm_logic_vector_array_mfb::seqv_no_inframe_gap_rx #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)::get_type());
+        this.add_sequence(uvm_logic_vector_array_mfb::sequence_stop_rx #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)::get_type());
+        this.add_sequence(uvm_logic_vector_array_mfb::sequence_burst_pcie_rx #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)::get_type());
+    endfunction
+endclass
+
