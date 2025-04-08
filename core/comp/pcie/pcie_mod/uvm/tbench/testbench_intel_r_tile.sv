b0f781f013b59550cee126902308dab985bc86b9
Radek Isa <isa@cesnet.cz>
2025-04-08 13:05:19 +0200

    fix(pcie-moud-uvm): change assign to force in uvm verification
        assing result in undefined value

--- core/comp/pcie/pcie_mod/uvm/tbench/testbench_intel_r_tile.sv
+++ core/comp/pcie/pcie_mod/uvm/tbench/testbench_intel_r_tile.sv
@@ -240,7 +240,9 @@ module testbench;
             wire logic [3*4-1 : 0] down_data_update_cnt;
             wire logic [3  -1 : 0] down_data_init_ack;
 
-            assign DUT_U.VHDL_DUT_U.pcie_core_i.pcie_clk[pcie_e] = PCIE_USER_CLK;
+            initial begin
+                force DUT_U.VHDL_DUT_U.pcie_core_i.pcie_clk[pcie_e] = PCIE_USER_CLK;
+            end
             assign DUT_U.VHDL_DUT_U.pcie_core_i.pcie_link_up_comb[pcie_e]  = '1;
 
             for (genvar pcie_r = 0; pcie_r < CQ_MFB_REGIONS; pcie_r++) begin
