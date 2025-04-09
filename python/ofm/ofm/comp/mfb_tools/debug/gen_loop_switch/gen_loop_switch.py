a93792d4e4d7f838f128d4cd95f1dd3fa98eaa5e
Daniel Kondys <xkondy00@vutbr.cz>
2025-04-09 16:48:39 +0200

    feat(python-ofm): add the ofm-gls commandline tool

--- python/ofm/ofm/comp/mfb_tools/debug/gen_loop_switch/gen_loop_switch.py
+++ python/ofm/ofm/comp/mfb_tools/debug/gen_loop_switch/gen_loop_switch.py
@@ -4,10 +4,13 @@
 
 from dataclasses import dataclass
 from typing import Optional, Tuple
+import argparse
+from tabulate import tabulate
 
 import nfb
 from ofm.comp.mfb_tools.debug.generator import MfbGenerator
 from ofm.comp.mfb_tools.logic.speed_meter import SpeedMeter
+from ofm.utils import convert_units
 
 @dataclass
 class StreamNodes:
@@ -247,3 +250,134 @@ class GenLoopSwitch(nfb.BaseComp):
     def sm_measure(sm: SpeedMeter, to: float = 0.1, freq: Optional[int] = 2*10**8) -> Tuple[float, float | None]:
         """Use the given SpeedMeter to measure the throughput speed, return [bps]."""
         return sm.measure(to, freq)
+
+
+def main():
+    help_dict = {
+        "device"   : "set the target device",
+        "index"    : "select index (instance) of the GLS in the Device Tree",
+        "right"    : "select the RIGHT side of the GLS to use the MFB Generator or set loopback",
+        "left"     : "select the LEFT side of the GLS to use the MFB Generator or set loopback",
+        "loopback" : "enable (1) or disable (0) loopback (on the '-R' or '-L' side)",
+        "generate" : "start (1) or stop (0) generating (to the '-R' or '-L' side), 'c' to print its config",
+        "size"     : "set frame size for the ('-R' or '-L') MFB Generator",
+        "measure"  : "measure the throughput using selected Speed Meter (SM), 'a' for all, '0,1' by default",
+        "config"   : "print full configuration and exit",
+    }
+
+    gls_desc = """
+        Communicates with the GEN_LOOP_SWITCH firmware component in the card to set
+        loopbacks, generate data, and measure throughput.
+    """
+
+    gls_diagram = r"""
+                                          Left2Right GLS Stream
+                                  ----------------------------------->>
+
+                                     +--------+  +---\
+                                     | RX Gen +--+ 1  \             +---\
+    ETH_RX  +------+                 +--------+  |MUX_C+------------+ 0  \   +------+  +------+  DMA_RX
+    >-------+ SM 2 +----------------------+------+ 0  /             |MUX_A+--+ FIFO +--+ SM 0 +------->
+            +------+                      |      +---/          +---+ 1  /   +------+  +------+
+                                          |                     |   +---/
+                                          |                     |
+     LEFT                                 |                     |                                RIGHT
+                                          |                     |
+                                  /---+   |                     |
+            +------+  +------+   /  1 +---+          /---+      |                      +------+
+    <-------+ SM 1 +--+ FIFO +--+MUX_B|             /  0 +------+----------------------+ SM 3 +-------<
+    ETH_TX  +------+  +------+   \  0 +------------+MUX_D|  +-----------------+        +------+  DMA_TX
+                                  \---+             \  1 +--+ TX Gen / Player |
+                                                     \---+  +-----------------+
+
+                                          Right2Left GLS Stream
+                                  <<-----------------------------------
+    """
+
+    arg_parser = argparse.ArgumentParser(
+        prog="ofm-gls",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=gls_desc + "\n" + gls_diagram
+    )
+    arg_parser.add_argument("-d", "--device", default=nfb.default_dev_path, help=help_dict["device"])
+    arg_parser.add_argument("-i", "--index", type=int, default=0, help=help_dict["index"])
+    arg_parser.add_argument("-R", "--right", action="store_true", help=help_dict["right"])
+    arg_parser.add_argument("-L", "--left", action="store_true", help=help_dict["left"])
+    arg_parser.add_argument("-l", "--loopback", type=int, choices=[0, 1], help=help_dict["loopback"])
+    arg_parser.add_argument("-g", "--generate", nargs="?", choices=["0", "1", "c"], help=help_dict["generate"])
+    arg_parser.add_argument("-s", "--size", type=int, help=help_dict["size"])
+    arg_parser.add_argument("-m", "--measure", nargs='?', const="default", choices=["default", "0", "1", "2", "3", "a"], help=help_dict["measure"])
+    arg_parser.add_argument("-c", "--config", action="store_true", help=help_dict["config"])
+    args = arg_parser.parse_args()
+
+    try:
+        gls = GenLoopSwitch(dev=nfb.open(args.device), index=args.index)
+    except IndexError:
+        raise IndexError("Could not open the GenLoopSwitch component (FW).")
+
+    if args.config:
+        print("Multiplexer configuration:")
+        mux_conf = []
+        mux_conf.append(["MUX A", f"  {gls.l2r.mux_loopback}"])
+        mux_conf.append(["MUX B", f"  {gls.r2l.mux_loopback}"])
+        mux_conf.append(["MUX C", f"  {gls.l2r.mux_generator}"])
+        mux_conf.append(["MUX D", f"  {gls.r2l.mux_generator}"])
+        print(tabulate(mux_conf))
+        print("MFB Generator Left (<-) configuration:")
+        print(tabulate(gls.l2r.gen.get_fconfiguration()))
+        print("MFB Generator Right (->) configuration:")
+        print(tabulate(gls.r2l.gen.get_fconfiguration()))
+        return
+
+    if args.measure is not None and gls.speedmeters_exist:
+        speed_meters = [gls.l2r.tx_sm, gls.r2l.tx_sm, gls.l2r.rx_sm, gls.r2l.rx_sm]
+        if args.measure == "default":
+            selected_id = [0, 1]
+        elif args.measure == "a":
+            selected_id = [0, 1, 2, 3]
+        else:
+            selected_id = [*map(int, args.measure)]
+        table_rows = []
+        for id in selected_id:
+            sm = speed_meters[id]
+            sm.clear_data()
+            bps, pps = GenLoopSwitch.sm_measure(sm)
+            b_speed, b_unit = convert_units(bps)
+            sm_row = [f"Speed meter {id}"]
+            sm_row.append(b_speed)
+            sm_row.append(f"{b_unit}bps")
+            if pps is not None:
+                p_speed, p_unit = convert_units(pps)
+                sm_row.append(p_speed)
+                sm_row.append(f"{p_unit}pps")
+            table_rows.append(sm_row)
+        print(tabulate(table_rows, floatfmt=".2f", colalign=("right","right","right")))
+
+    # GLS Stream selection
+    streams = []
+    if args.right:
+        streams.append(gls.l2r)
+    if args.left:
+        streams.append(gls.r2l)
+
+    for s in streams:
+        if args.loopback is not None:
+            s.mux_loopback = args.loopback
+
+        if args.size is not None:
+            s.gen.frame_length = args.size
+
+        # Start/stop the generator and/or print its configuration
+        if args.generate:
+            if "1" in args.generate:
+                s.gen_start(en_path=True)
+            elif "0" in args.generate:
+                s.gen_stop()
+
+            if "c" in args.generate:
+                print("MFB Generator configuration:")
+                print(tabulate(s.gen.get_fconfiguration()))
+
+
+if __name__ == "__main__":
+    main()
