85619a68ba01525e58b32c53e022604cea3d6a00
Tomas Fukac <fukac@dyna-nic.com>
2025-04-10 13:57:54 +0200

    fix(card-agi-fh400g): fix power management settings for board revision 2

--- cards/reflexces/agi-fh400g/src/Quartus.inc.tcl
+++ cards/reflexces/agi-fh400g/src/Quartus.inc.tcl
@@ -63,14 +63,16 @@ set SYNTH_FLAGS(CONSTR) ""
 set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/timing.sdc"
 if {$BOARD_REV == 0} {
     set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/device_r0.qsf"
+    set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/general-rev0-1.qsf"
 }
 if {$BOARD_REV == 1} {
     set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/device_r3.qsf"
+    set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/general-rev0-1.qsf"
 }
 if {$BOARD_REV == 2} {
     set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/device.qsf"
+    set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/general-rev2.qsf"
 }
-set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/general.qsf"
 set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/hps.qsf"
 set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/sodimm.qsf"
 set SYNTH_FLAGS(CONSTR) "$SYNTH_FLAGS(CONSTR) $CARD_BASE/constr/pcie_misc.qsf"
