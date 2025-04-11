0ee392dd361ce9c7638a6f1f946e199dec00028a
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-04-11 20:13:16 +0200

    feat(cocotb - tx_mac_lite): add cocotb test for tx_mac_lite adapter

---
+++ comp/nic/mac_lite/tx_mac_lite/comp/adapters/mac_seg/cocotb/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+NDK_FPGA_PATH=../../../../../../../..
+
+source $NDK_FPGA_PATH/env.sh
+
+ndk_fpga_venv_prepare "venv-tx_mac_seg"
+
+pip install -e .
+#pip install "cocotbext-ofm[nfb]@$NDK_FPGA_COCOTBEXT_OFM_URL"
