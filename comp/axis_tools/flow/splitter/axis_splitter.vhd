b35990578a0e283130098f7f783875175adaeea7
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-05-02 16:45:36 +0200

    feat(axis tools): implement axis splitter

--- comp/axis_tools/flow/splitter/axis_splitter.vhd
+++ comp/axis_tools/flow/splitter/axis_splitter.vhd
@@ -1,9 +1,6 @@
--- axis_splitter.vhd: AXI-Stream splitter
--- Copyright (C) 2024 CESNET
--- Author(s): Ondřej Schwarz <Ondrej.Schwarz@cesnet.cz>
---
 -- SPDX-License-Identifier: BSD-3-Clause
---
+-- Copyright (C) 2025 CESNET z. s. p. o.
+-- Author(s): Ondrej Schwarz <ondrejschwarz@cesnet.cz>
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -24,9 +21,11 @@ generic (
     -- width of AXI-Stream user signal in bits
     TUSER_WIDTH   : natural := 64;
     -- number of TX AXI-Stream interfaces
-    TX_STREAMS    : natural := 512;
+    TX_STREAMS    : natural := 32;
     -- target device: AGILEX, STRATIX10, ULTRASCALE,...
-    DEVICE        : string  := "AGILEX"
+    DEVICE        : string  := "AGILEX";
+    -- adds register to the output
+    OUT_REG       : boolean := true
 );
 port (
     -- =========================================================================
@@ -56,14 +55,68 @@ port (
     TX_AXIS_TKEEP  : out slv_array_t(TX_STREAMS-1 downto 0)(TDATA_WIDTH/8-1 downto 0);
     TX_AXIS_TLAST  : out std_logic_vector(TX_STREAMS-1 downto 0);
     TX_AXIS_TVALID : out std_logic_vector(TX_STREAMS-1 downto 0);
-    TX_AXIS_TREADY : in  std_logic_vector(TX_STREAMS-1 downto 0);
+    TX_AXIS_TREADY : in  std_logic_vector(TX_STREAMS-1 downto 0)
 );
 end entity;
 
 architecture FULL of AXIS_SPLITTER is
+    signal data      : std_logic_vector(TDATA_WIDTH-1 downto 0);
+    signal user      : std_logic_vector(TUSER_WIDTH-1 downto 0);
+    signal keep      : std_logic_vector(TDATA_WIDTH/8-1 downto 0);
+    signal last      : std_logic;
+    signal valid     : std_logic_vector(TX_STREAMS-1 downto 0);
+    signal ready     : std_logic;
 
 begin
+    -- =========================================================================
+    -- Input
+    -- =========================================================================
+    RX_AXIS_TREADY <= ready;
 
-    -- TODO @OndřejSchwarz
+    -- =========================================================================
+    -- Splitter logic
+    -- =========================================================================
+    data  <= RX_AXIS_TDATA;
+    user  <= RX_AXIS_TUSER;
+    keep  <= RX_AXIS_TKEEP;
+    last  <= RX_AXIS_TLAST;
+    ready <= TX_AXIS_TREADY(to_integer(unsigned(RX_AXIS_SEL)));
 
+    set_valid: process(RX_AXIS_SEL, RX_AXIS_TVALID)
+    begin
+        valid <= (others => '0');
+        valid(to_integer(unsigned(RX_AXIS_SEL))) <= RX_AXIS_TVALID;
+    end process;
+
+    -- =========================================================================
+    -- Output
+    -- =========================================================================
+    register_g: if OUT_REG generate
+        reg_g: for g in 0 to TX_STREAMS-1 generate
+            process(CLK)
+            begin
+                if rising_edge(CLK) then
+                    if TX_AXIS_TREADY(g) = '1' then
+                        TX_AXIS_TDATA(g)  <= data;
+                        TX_AXIS_TUSER(g)  <= user;
+                        TX_AXIS_TKEEP(g)  <= keep;
+                        TX_AXIS_TLAST(g)  <= last;
+                        TX_AXIS_TVALID(g) <= valid(g);
+                    end if;
+
+                    if RESET = '1' then
+                        TX_AXIS_TVALID(g) <= '0';
+                    end if;
+                end if;
+            end process;
+        end generate;
+    else generate
+        signal_splitter_g: for g in 0 to TX_STREAMS-1 generate
+            TX_AXIS_TDATA(g)  <= data;
+            TX_AXIS_TUSER(g)  <= user;
+            TX_AXIS_TKEEP(g)  <= keep;
+            TX_AXIS_TLAST(g)  <= last;
+            TX_AXIS_TVALID(g) <= valid(g);
+        end generate;
+    end generate;
 end architecture;
