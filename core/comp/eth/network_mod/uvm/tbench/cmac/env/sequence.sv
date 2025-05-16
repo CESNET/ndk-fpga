850b92b14d14d501ce5e0d08fd3aacee3a061326
Yaroslav Marushchenko <xmarus09@stud.fit.vutbr.cz>
2025-05-16 13:29:37 +0200

    fix(netmod): add a delay to prevent sending the counter reset command too soon in the verification

--- core/comp/eth/network_mod/uvm/tbench/cmac/env/sequence.sv
+++ core/comp/eth/network_mod/uvm/tbench/cmac/env/sequence.sv
@@ -165,6 +165,8 @@ class virt_sequence_port #(
         seq_sync_usr_rx.send_stop();
         seq_sync_eth_tx.send_stop();
 
+        #(300ns);
+
         // Read statistics
         for (int unsigned it = 0; it < ETH_PORT_CHAN; it++) begin
             uvm_network_mod_env::read_rx_counters#(RX_MAC_COUNT) rx_stats;
@@ -187,6 +189,8 @@ class virt_sequence_port #(
                 rx_stats.reset();
                 tx_stats.reset();
             join
+
+            #(40ns);
 
             fork
                 rx_stats.start(null);
