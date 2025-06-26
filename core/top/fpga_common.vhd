2d35182afb5704775c9e70246a6e01cff4b22bc4
Vladislav Valek <vladislav.valek@email.cz>
2025-06-26 11:48:49 +0200

    style(fpga-common): remove unused function
    This function has not been used anywhere and was therefore deleted.

--- core/top/fpga_common.vhd
+++ core/top/fpga_common.vhd
@@ -360,18 +360,6 @@ architecture FULL of FPGA_COMMON is
 
     constant HDR_META_WIDTH     : integer := 12;
 
-    -- This function returns appropriate REGION_SIZE parameter value according to set DMA type, and
-    -- PCIe configuration
-    function mfb_reg_size_calc_f
-        return natural is
-    begin
-        if (DEVICE = "ULTRASCALE" and DMA_TYPE = 4 and PCIE_ENDPOINTS = 1 and PCIE_ENDPOINT_MODE = 2) then
-            return 4;
-        end if;
-
-        return 8;
-    end function;
-
     function pcie_mfb_regions_calc_f (PCIE_DIR : string) return natural is
         variable pcie_mfb_regions : natural;
     begin
