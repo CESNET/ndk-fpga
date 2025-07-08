0e8f98a69a5eeba292ce1066a2c4f11b4cc8467d
Yaroslav Marushchenko <xmarus09@stud.fit.vutbr.cz>
2025-07-08 15:16:03 +0200

    feat(uvm): check if unmasked and original interfaces behave correctly in frame masker verification

--- comp/mfb_tools/flow/frame_masker/uvm/tbench/dut.sv
+++ comp/mfb_tools/flow/frame_masker/uvm/tbench/dut.sv
@@ -12,7 +12,9 @@ module DUT (
     input logic   RST,
     mfb_if.dut_rx mfb_rx,
     mvb_if.dut_rx mvb_rx,
-    mfb_if.dut_tx mfb_tx
+    mfb_if.dut_tx mfb_tx,
+    mfb_if.dut_tx mfb_unmasked_tx,
+    mfb_if.dut_tx mfb_original_tx
 );
     logic tx_src_rdy;
     logic tx_dst_rdy;
@@ -25,41 +27,54 @@ module DUT (
         .META_WIDTH  (MFB_META_WIDTH ),
         .USE_PIPE    (USE_PIPE       )
     ) VHDL_DUT_U (
-        .CLK             (CLK),
-        .RESET           (RST),
+        .CLK                 (CLK),
+        .RESET               (RST),
 
-        .RX_DATA         (mfb_rx.DATA   ),
-        .RX_META         (mfb_rx.META   ),
-        .RX_SOF_POS      (mfb_rx.SOF_POS),
-        .RX_EOF_POS      (mfb_rx.EOF_POS),
-        .RX_SOF          (mfb_rx.SOF    ),
-        .RX_EOF          (mfb_rx.EOF    ),
-        .RX_SRC_RDY      (mfb_rx.SRC_RDY),
-        .RX_DST_RDY      (mfb_rx.DST_RDY),
+        .RX_DATA             (mfb_rx.DATA   ),
+        .RX_META             (mfb_rx.META   ),
+        .RX_SOF_POS          (mfb_rx.SOF_POS),
+        .RX_EOF_POS          (mfb_rx.EOF_POS),
+        .RX_SOF              (mfb_rx.SOF    ),
+        .RX_EOF              (mfb_rx.EOF    ),
+        .RX_SRC_RDY          (mfb_rx.SRC_RDY),
+        .RX_DST_RDY          (mfb_rx.DST_RDY),
 
-        .TX_DATA         (mfb_tx.DATA   ),
-        .TX_META         (mfb_tx.META   ),
-        .TX_SOF_POS      (mfb_tx.SOF_POS),
-        .TX_EOF_POS      (mfb_tx.EOF_POS),
-        .TX_SOF_MASKED   (mfb_tx.SOF    ),
-        .TX_EOF_MASKED   (mfb_tx.EOF    ),
-        .TX_SRC_RDY      (tx_src_rdy    ),
-        .TX_DST_RDY      (tx_dst_rdy    ),
+        .TX_DATA             (mfb_tx.DATA   ),
+        .TX_META             (mfb_tx.META   ),
+        .TX_SOF_POS          (mfb_tx.SOF_POS),
+        .TX_EOF_POS          (mfb_tx.EOF_POS),
+        .TX_SOF_MASKED       (mfb_tx.SOF    ),
+        .TX_EOF_MASKED       (mfb_tx.EOF    ),
+        .TX_SRC_RDY          (tx_src_rdy    ),
+        .TX_DST_RDY          (tx_dst_rdy    ),
 
-        // .TX_SOF_UNMASKED (mfb_tx.SOF    ),
-        // .TX_EOF_UNMASKED (mfb_tx.EOF    ),
-        // .TX_SRC_RDY      (mfb_tx.SRC_RDY),
+        .TX_SOF_UNMASKED     (mfb_unmasked_tx.SOF    ),
+        .TX_EOF_UNMASKED     (mfb_unmasked_tx.EOF    ),
+        .TX_SRC_RDY_UNMASKED (mfb_unmasked_tx.SRC_RDY),
 
-        // .TX_SOF_ORIGINAL (mfb_tx.SOF    ),
-        // .TX_EOF_ORIGINAL (mfb_tx.EOF    ),
-        // .TX_SRC_RDY      (mfb_tx.SRC_RDY),
+        .TX_SOF_ORIGINAL     (mfb_original_tx.SOF    ),
+        .TX_EOF_ORIGINAL     (mfb_original_tx.EOF    ),
+        .TX_SRC_RDY_ORIGINAL (mfb_original_tx.SRC_RDY),
 
-        .TX_MASK         (mvb_rx.DATA   )
+        .TX_MASK             (mvb_rx.DATA)
     );
 
     assign mfb_tx.SRC_RDY = tx_src_rdy & mvb_rx.SRC_RDY & mvb_rx.VLD;
     assign tx_dst_rdy = mfb_tx.DST_RDY & mvb_rx.SRC_RDY & mvb_rx.VLD;
 
     assign mvb_rx.DST_RDY = mfb_tx.DST_RDY;
+
+    // Only the control signals (SOF, EOF, SRC_RDY and DST_RDY) matter.
+    assign mfb_unmasked_tx.DATA    = 'b0;
+    assign mfb_unmasked_tx.META    = 'b0;
+    assign mfb_unmasked_tx.SOF_POS = 'b0;
+    assign mfb_unmasked_tx.EOF_POS = 'b0;
+    assign mfb_unmasked_tx.DST_RDY = tx_dst_rdy;
+
+    assign mfb_original_tx.DATA    = 'b0;
+    assign mfb_original_tx.META    = 'b0;
+    assign mfb_original_tx.SOF_POS = 'b0;
+    assign mfb_original_tx.EOF_POS = 'b0;
+    assign mfb_original_tx.DST_RDY = tx_dst_rdy;
 
 endmodule
--- comp/mfb_tools/flow/frame_masker/uvm/tbench/testbench.sv
+++ comp/mfb_tools/flow/frame_masker/uvm/tbench/testbench.sv
@@ -22,10 +22,12 @@ module testbench;
 
     // -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     // Interfaces
-    reset_if                                                                               reset (CLK);
-    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_rx(CLK);
-    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_tx(CLK);
-    mvb_if #(1, MFB_REGIONS)                                                               mvb_rx(CLK);
+    reset_if                                                                               reset          (CLK);
+    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_rx         (CLK);
+    mvb_if #(1, MFB_REGIONS)                                                               mvb_rx         (CLK);
+    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_tx         (CLK);
+    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_unmasked_tx(CLK);
+    mfb_if #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) mfb_original_tx(CLK);
 
     bind MFB_FRAME_MASKER : DUT_U.VHDL_DUT_U probe_inf#(REGIONS*2) probe_mask2discard(TX_DST_RDY & TX_SRC_RDY_UNMASKED, {TX_SOF_UNMASKED, TX_MASK}, CLK);
 
@@ -58,11 +60,13 @@ module testbench;
     // -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     // DUT
     DUT DUT_U (
-        .CLK    (CLK        ),
-        .RST    (reset.RESET),
-        .mfb_rx (mfb_rx     ),
-        .mvb_rx (mvb_rx     ),
-        .mfb_tx (mfb_tx     )
+        .CLK             (CLK            ),
+        .RST             (reset.RESET    ),
+        .mfb_rx          (mfb_rx         ),
+        .mvb_rx          (mvb_rx         ),
+        .mfb_tx          (mfb_tx         ),
+        .mfb_unmasked_tx (mfb_unmasked_tx),
+        .mfb_original_tx (mfb_original_tx)
     );
 
     // Properties
@@ -74,10 +78,12 @@ module testbench;
         .MFB_META_WIDTH  (MFB_META_WIDTH )
     )
     PROPERTY_CHECK (
-        .RESET      (reset.RESET),
-        .tx_mfb_vif (mfb_tx     ),
-        .rx_mfb_vif (mfb_rx     ),
-        .mvb_vif    (mvb_rx     )
+        .RESET               (reset.RESET    ),
+        .tx_mfb_vif          (mfb_tx         ),
+        .tx_mfb_unmasked_vif (mfb_unmasked_tx),
+        .tx_mfb_original_vif (mfb_original_tx),
+        .rx_mfb_vif          (mfb_rx         ),
+        .mvb_vif             (mvb_rx         )
     );
 
 endmodule
