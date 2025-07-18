ac701b77417eedfcec3d52033aa2817287418f2d
Martin Spinler <spinler@cesnet.cz>
2025-07-18 16:36:00 +0200

    fix(ci - pycodestyle): use exitcode of all commands, not only last

--- tests/ci/pycodestyle.sh
+++ tests/ci/pycodestyle.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-flake8 --extend-exclude "ver_settings.py,synth_settings.py,extra/" --statistics --extend-ignore=T201,T202,B902,F821,E501,E203,E221,E261,E265,E266
-mypy python/ofm --config-file=python/ofm/pyproject.toml
+flake8 --extend-exclude "ver_settings.py,synth_settings.py,extra/" --statistics --extend-ignore=T201,T202,B902,F821,E501,E203,E221,E261,E265,E266 || failed=1
+mypy python/ofm --config-file=python/ofm/pyproject.toml || failed=1
+
+if [ ${failed:-0} -eq 1 ]; then
+	exit 1
+fi
