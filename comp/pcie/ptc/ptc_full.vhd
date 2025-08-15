0318dc2a10030d2433cefca369f5dd41fe5a1111
Jakub Cabal <cabal@cesnet.cz>
2025-08-15 10:18:06 +0200

    fix(PTC): fix HDM_MFB_FIFO_DEPTH calculation, MPS is in dwords

--- comp/pcie/ptc/ptc_full.vhd
+++ comp/pcie/ptc/ptc_full.vhd
@@ -50,7 +50,7 @@ architecture FULL of PCIE_TRANSACTION_CTRL is
     constant PCIE_LOW_ADDR_WIDTH      : integer := getpcielowaddrwidth;
 
     -- HDM (Header Data Merge) MFB FIFO is ready for 16 MPS transaction
-    constant HDM_MFB_FIFO_DEPTH       : integer := (16*MPS*8)/MFB_UP_WIDTH;
+    constant HDM_MFB_FIFO_DEPTH       : integer := (16*MPS*32)/MFB_UP_WIDTH;
     -- CODAPA counter must hold all transaction (1 transaction per region) in HDM MFB FIFO
     constant CODAPA_CNT_WIDTH         : integer := log2(MFB_UP_REGIONS*HDM_MFB_FIFO_DEPTH)+1;
 
