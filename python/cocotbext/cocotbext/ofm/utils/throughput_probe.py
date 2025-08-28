43d339e811fee82b9221c8ca7f50ca42e3cfb471
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-08-28 16:58:11 +0200

    fix(cocotb - utils): add the ability to set custom log name to ThroughputProbe

--- python/cocotbext/cocotbext/ofm/utils/throughput_probe.py
+++ python/cocotbext/cocotbext/ofm/utils/throughput_probe.py
@@ -97,8 +97,9 @@ class ThroughputProbe(Probe):
         - To get the average throughput and efficiency, call the log_average_throughput class.
           This is usually done at the end of the test.
     """
-    def __init__(self, interface: ThroughputProbeInterface = None, period: int = 0, throughput_units: str = "items", time_units: str = "us", log_intervals: list | None = None, callback=None):
-        super().__init__(interface, period, time_units, log_intervals, callback)
+    def __init__(self, interface: ThroughputProbeInterface = None, period: int = 0, throughput_units: str = "items", time_units: str = "us", log_intervals: list | None = None,
+                 name: str | None = None, callback=None):
+        super().__init__(interface, period, time_units, log_intervals, name, callback)
         self._total_item_cnt = 0
         self._clear_log_values()
         self._throughput_units = throughput_units.lower()
