51167518aa8c208ac4eb36a23def47e332008a54
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2025-08-31 04:57:23 +0200

    chore(cocotb - prepare.sh): remove all prepare.sh scripts

--- apps/minimal/tests/cocotb/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NDK_FPGA_PATH=../../../..
-source $NDK_FPGA_PATH/env.sh
-
-ndk_fpga_venv_prepare "venv-cocotb"
-
-pip install .
-#pip install "cocotbext-ofm[nfb]@$NDK_FPGA_COCOTBEXT_OFM_URL"
