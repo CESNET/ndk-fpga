33c0ae8d61e8f134d7b3bd847dc2f78bf2c7e2e4
Stepan Friedl <friedl@cesnet.cz>
2025-09-25 08:20:29 +0200

    feat(rx_mac_lite): improve link status decoding in MII adapter - any RX sequence causes link down

--- comp/nic/mac_lite/rx_mac_lite/comp/adapters/umii_dec/umii_dec.vhd
+++ comp/nic/mac_lite/rx_mac_lite/comp/adapters/umii_dec/umii_dec.vhd
@@ -82,7 +82,7 @@ architecture FULL of UMII_DEC is
     signal mii_vld_in                       : std_logic;
 
     -- logic stage 1
-    signal s_is_locfault                     : std_logic_vector(REGIONS-1 downto 0);
+    signal s_is_sequence                     : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_preamble                     : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_terminate                    : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_error                        : std_logic_vector(REGIONS-1 downto 0);
@@ -92,7 +92,7 @@ architecture FULL of UMII_DEC is
     -- register stage 1
     signal s_mii_rxd_reg1                    : std_logic_vector(MII_DW-1 downto 0);
     signal s_mii_rxc_reg1                    : std_logic_vector(MII_CW-1 downto 0);
-    signal s_is_locfault_reg1                : std_logic_vector(REGIONS-1 downto 0);
+    signal s_is_sequence_reg1                : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_preamble_reg1                : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_terminate_reg1               : std_logic_vector(REGIONS-1 downto 0);
     signal s_is_error_reg1                   : std_logic_vector(REGIONS-1 downto 0);
@@ -270,7 +270,10 @@ begin
             MII_RXD       => mii_rxd_in((r+1)*REGION_WIDTH-1 downto r*REGION_WIDTH),
             MII_RXC       => mii_rxc_in((r+1)*REGION_ITEMS-1 downto r*REGION_ITEMS),
 
-            IS_LOCFAULT   => s_is_locfault(r),
+            IS_LOCFAULT   => open,
+            IS_REMFAULT   => open,
+            IS_LINKINT    => open,
+            IS_SEQUENCE   => s_is_sequence(r),
             IS_PREAMBLE   => s_is_preamble(r),
             IS_START      => open,
             IS_TERMINATE  => s_is_terminate(r),
@@ -293,7 +296,7 @@ begin
         if (rising_edge(CLK)) then
             s_mii_rxd_reg1       <= mii_rxd_in;
             s_mii_rxc_reg1       <= mii_rxc_in;
-            s_is_locfault_reg1   <= s_is_locfault;
+            s_is_sequence_reg1   <= s_is_sequence;
             s_is_preamble_reg1   <= s_is_preamble;
             s_is_terminate_reg1  <= s_is_terminate;
             s_is_error_reg1      <= s_is_error;
@@ -318,7 +321,7 @@ begin
     -- =========================================================================
 
     -- On link is error
-    s_link_error <= (or s_is_error_reg1) or (or s_is_locfault_reg1);
+    s_link_error <= (or s_is_error_reg1) or (or s_is_sequence_reg1);
 
     -- First and last Terminate position
     terminate_first_last_g : for r in 0 to REGIONS-1 generate
