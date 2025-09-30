24e42cc03f48b34d5cd5e57aa9e2a0d19e97ef22
Radek Isa <isa@cesnet.cz>
2025-09-30 22:27:34 +0200

    feat(network_mod-uvm): lower number of transaction in verification

--- core/comp/eth/network_mod/uvm/tbench/e-tile/env/sequence.sv
+++ core/comp/eth/network_mod/uvm/tbench/e-tile/env/sequence.sv
@@ -393,7 +393,7 @@ class virt_sequence_simple #(ETH_PORTS, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, ITEM
         end
 
         //SEND STOP
-        wait (transactions >= ETH_PORTS*30_000);
+        wait (transactions >= ETH_PORTS*20_000);
         seq_sync_port_end.send_stop();
         for (int unsigned it = 0; it < ETH_PORTS; it++) begin
             wait(port_end[it] == 1);
--- core/comp/eth/network_mod/uvm/tbench/f-tile/env/sequence.sv
+++ core/comp/eth/network_mod/uvm/tbench/f-tile/env/sequence.sv
@@ -428,7 +428,7 @@ class virt_sequence_simple #(
         end
 
         //SEND STOP
-        wait (transactions >= ETH_PORTS*30_000);
+        wait (transactions >= ETH_PORTS*20_000);
         seq_sync_port_end.send_stop();
         for (int unsigned it = 0; it < ETH_PORTS; it++) begin
             wait(port_end[it] == 1);
