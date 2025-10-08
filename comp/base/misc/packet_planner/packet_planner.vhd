d287d6352f3c1d27a6441cf42e9b9bd31a1816d5
Jakub Cabal <cabal@cesnet.cz>
2025-10-08 15:29:45 +0200

    perf(packet-planner): adjust FIFO capacity for smoother running

--- comp/base/misc/packet_planner/packet_planner.vhd
+++ comp/base/misc/packet_planner/packet_planner.vhd
@@ -154,7 +154,7 @@ architecture FULL of PACKET_PLANNER is
     -- arriving to the FIFO input and be propagated to output.
     -- The FIFO must contain enough items to compensate this time,
     -- so VLD does not drop.
-    constant OUT_FIFO_ITEMS        : natural := PLANNED_PKTS*32;
+    constant OUT_FIFO_ITEMS        : natural := PLANNED_PKTS*64;
     constant OUT_FIFO_AFULL_OFFSET : natural := 16*PLANNED_PKTS;
 
     -- =====================================================================
