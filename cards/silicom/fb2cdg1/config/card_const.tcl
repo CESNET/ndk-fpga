80ea58519ed022af866708550f989c3c12fb0f82
David Benes <benes@dyna-nic.com>
2025-10-24 15:12:56 +0200

    feat(cards - fb2cdg1): enable build for pcie gen5x16

--- cards/silicom/fb2cdg1/config/card_const.tcl
+++ cards/silicom/fb2cdg1/config/card_const.tcl
@@ -34,10 +34,12 @@ set QSFP_I2C_ADDR(1) "0xA0"
 # ------------------------------------------------------------------------------
 
 if {!(($PCIE_ENDPOINTS == 1 && $PCIE_GEN == 4 && $PCIE_ENDPOINT_MODE == 0) ||
+      ($PCIE_ENDPOINTS == 1 && $PCIE_GEN == 5 && $PCIE_ENDPOINT_MODE == 0) ||
       ($PCIE_ENDPOINTS == 2 && $PCIE_GEN == 5 && $PCIE_ENDPOINT_MODE == 1) )} {
     error "Incompatible PCIe configuration: PCIE_ENDPOINTS = $PCIE_ENDPOINTS, PCIE_GEN = $PCIE_GEN, PCIE_ENDPOINT_MODE = $PCIE_ENDPOINT_MODE!
 Allowed PCIe configurations:
 - 1xGen4x16  -- PCIE_GEN=4, PCIE_ENDPOINTS=1, PCIE_ENDPOINT_MODE=0 (for DMA Calypte only)
+- 1xGen5x16  -- PCIE_GEN=5, PCIE_ENDPOINTS=1, PCIE_ENDPOINT_MODE=0 (for DMA Medusa only)
 - 1xGen5x8x8 -- PCIE_GEN=5, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1 (for DMA Medusa only)"
 }
 
