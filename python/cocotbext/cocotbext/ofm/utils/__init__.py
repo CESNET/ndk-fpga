6a785a71baab13138f022a1c0b7be6aae397469e
Martin Spinler <spinler@cesnet.cz>
2025-11-05 15:50:58 +0100

    feat(cocotbext): add 'partial' wrapper as hotfix for use in cocotb_bus.Monitor callbacks

--- python/cocotbext/cocotbext/ofm/utils/__init__.py
+++ python/cocotbext/cocotbext/ofm/utils/__init__.py
@@ -1,5 +1,6 @@
 from .header import SerializableHeader, concat, deconcat, byte_serialize, byte_deserialize
 from .ram import RAM
 from .math import numberOfSetBits, bitmask
+from .fixes import partial
 
-__all__ = ["SerializableHeader", "concat", "deconcat", "RAM", "numberOfSetBits", "bitmask", "byte_serialize", "byte_deserialize"]
+__all__ = ["SerializableHeader", "concat", "deconcat", "RAM", "numberOfSetBits", "bitmask", "byte_serialize", "byte_deserialize", "partial"]
