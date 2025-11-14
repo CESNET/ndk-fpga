4ff56e458010bd6eda1fa5e66c856f30e964fa9a
Radek Isa <isa@cesnet.cz>
2025-11-14 16:50:04 +0100

    feat(uvm-pcie): create UVC pcie to axi

---
+++ comp/uvm/pcie_axi/pkg.sv
@@ -0,0 +1,30 @@
+// pkg.sv: Package for pcie
+// Copyright (C) 2024 CESNET z. s. p. o.
+// Author:  Radek Iša <isa@cesnet.cz>
+
+// SPDX-License-Identifier: BSD-3-Clause
+
+`ifndef PCIE_AXI_ENV_SV
+`define PCIE_AXI_ENV_SV
+
+package uvm_pcie_axi;
+
+    `include "uvm_macros.svh"
+    import uvm_pkg::*;
+
+
+    `include "config.sv"
+    `include "fce.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+
+    `include "sequence_cq.sv"
+    `include "sequence_cc.sv"
+    `include "sequence_rq.sv"
+    `include "sequence_rc.sv"
+    `include "env.sv"
+    `include "root.sv"
+
+endpackage
+
+`endif
