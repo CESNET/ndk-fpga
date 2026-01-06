cfd3ac5d6d7f8d94f37c059152a87d3708d5e0a7
Vlastimil Kosar <kosar@dyna-nic.com>
2026-01-06 20:19:50 +0100

    fix(cards-fb2cghh): use absolute path for DDR4 parameters file

--- cards/silicom/fb2cghh/src/ip/ddr4_axi/ddr4_axi.ip.tcl
+++ cards/silicom/fb2cghh/src/ip/ddr4_axi/ddr4_axi.ip.tcl
@@ -17,7 +17,7 @@ set config_list [list \
     CONFIG.C0.DDR4_AxiSelection {true} \
     CONFIG.C0.DDR4_CLKFBOUT_MULT {5} \
     CONFIG.C0.DDR4_CasLatency {22} \
-    CONFIG.C0.DDR4_CustomParts [subst $PARAMS(IP_MODIFY_BASE)/fb2cghh_onboard_ddr4.csv] \
+    CONFIG.C0.DDR4_CustomParts [file normalize [subst $PARAMS(IP_MODIFY_BASE)/fb2cghh_onboard_ddr4.csv]] \
     CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} \
     CONFIG.C0.DDR4_DataWidth {72} \
     CONFIG.C0.DDR4_Ecc {true} \
