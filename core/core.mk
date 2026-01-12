36128bd9ba5b9dd57863a90f20ab99fc673f8747
Vladislav Valek <vladislav.valek@email.cz>
2026-01-12 15:05:28 +0100

    refactor(wip, make): remove card specific Makefile includes
    These were all the same so they were removed and their content moved to core.mk

--- core/core.mk
+++ core/core.mk
@@ -4,6 +4,18 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+# This value is set as default in SYNTH_FLAGS(OUTPUT)
+OUTPUT_NAME?=unknown-card
+USER_ENV ?=
+
+CORE_BASE?=$(dir $(lastword $(MAKEFILE_LIST)))
+
+PCIE_CONF?=1xGen3x16
+
+ETH_PORTS?=1
+ETH_PORT_SPEED?=100
+ETH_PORT_CHAN?=1
+
 # Supported DMA types (possible values: 0, 3, 4):
 # 0 - Disable DMA
 # 3 - DMA Medusa
@@ -16,8 +28,25 @@ NET_MOD_ENABLE?=true
 APP_CORE_ENABLE?=true
 BMC_ENABLE?=true
 
+include $(CORE_BASE)/ndk_paths.mk
+
 NETCOPE_ENV += \
+	OFM_PATH=$(OFM_PATH) \
+	COMBO_BASE=$(COMBO_BASE) \
+	FIRMWARE_BASE=$(FIRMWARE_BASE) \
+	CARD_BASE=$(CARD_BASE) \
+	CORE_BASE=$(CORE_BASE) \
+	APP_CONF=$(APP_CONF) \
+	OUTPUT_NAME=$(OUTPUT_NAME) \
+	ETH_PORTS=$(ETH_PORTS) \
+	ETH_PORT_SPEED=$(ETH_PORT_SPEED) \
+	ETH_PORT_CHAN=$(ETH_PORT_CHAN) \
+	DMA_TYPE=$(DMA_TYPE) \
 	DMA_DEBUG_ENABLE=$(DMA_DEBUG_ENABLE) \
 	BMC_ENABLE=$(BMC_ENABLE) \
 	NET_MOD_ENABLE=$(NET_MOD_ENABLE) \
-	APP_CORE_ENABLE=$(APP_CORE_ENABLE)
+	APP_CORE_ENABLE=$(APP_CORE_ENABLE) \
+	PCIE_CONF=$(PCIE_CONF) \
+	$(USER_ENV)
+
+include $(OFM_PATH)/build/Makefile.Vivado.inc
