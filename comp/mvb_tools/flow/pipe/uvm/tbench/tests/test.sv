b784eb762913633db919dd652237ed471a73bfd1
Radek Isa <isa@cesnet.cz>
2026-01-14 14:04:32 +0100

    feat(pipe-uvm): remove tx sequences

--- comp/mvb_tools/flow/pipe/uvm/tbench/tests/test.sv
+++ comp/mvb_tools/flow/pipe/uvm/tbench/tests/test.sv
@@ -10,7 +10,6 @@ class ex_test extends uvm_test;
 
     bit timeout;
     uvm_pipe::env #(ITEMS, ITEM_WIDTH)             m_env;
-    uvm_mvb::sequence_lib_tx#(ITEMS, ITEM_WIDTH)   h_seq_tx;
 
     // ------------------------------------------------------------------------
     // Functions
@@ -54,26 +53,8 @@ class ex_test extends uvm_test;
     endtask
 
 
-    task run_seq_tx(uvm_phase phase);
-        forever begin
-            h_seq_tx.randomize();
-            h_seq_tx.start(m_env.tx_env.m_mvb_agent.m_sequencer);
-        end
-    endtask
-
     virtual task run_phase(uvm_phase phase);
-
-        h_seq_tx = uvm_mvb::sequence_lib_tx #(ITEMS, ITEM_WIDTH)::type_id::create("h_seq_tx");
-        h_seq_tx.init_sequence();
-        h_seq_tx.cfg.probability_set(60, 100);
-        h_seq_tx.min_random_count = 200;
-        h_seq_tx.max_random_count = 500;
-
-
-        fork
-            run_seq_tx(phase);
-            run_seq_rx(phase);
-        join
+        run_seq_rx(phase);
     endtask
 
     function void report_phase(uvm_phase phase);
