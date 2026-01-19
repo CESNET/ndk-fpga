904e50124f23c9016fd440f150bbf7b1979c846d
Radek Isa <isa@cesnet.cz>
2026-01-19 17:51:29 +0100

    feat(merge_streams-uvm): remove tx sequence

--- comp/mvb_tools/flow/merge_streams_ordered/uvm/tbench/env/sequencer.sv
+++ comp/mvb_tools/flow/merge_streams_ordered/uvm/tbench/env/sequencer.sv
@@ -10,7 +10,6 @@ class virt_sequencer #(MVB_ITEMS, MVB_ITEM_WIDTH, RX_STREAMS) extends uvm_sequen
     uvm_reset::sequencer                                        m_reset_sqcr;
     uvm_logic_vector::sequencer #(MVB_ITEM_WIDTH)               m_rx_mvb_sqcr [RX_STREAMS -1 : 0];
     uvm_logic_vector::sequencer #($clog2(RX_STREAMS))           m_rx_sel_mvb_sqcr;
-    uvm_mvb::sequencer #(MVB_ITEMS*RX_STREAMS, MVB_ITEM_WIDTH)  m_tx_mvb_sqcr;
 
     function new(string name = "virt_sequencer", uvm_component parent);
         super.new(name, parent);
