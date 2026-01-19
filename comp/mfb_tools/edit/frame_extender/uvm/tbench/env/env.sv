773f2787ede88db44291496552f0c0fb28ce7a78
Radek Isa <isa@cesnet.cz>
2026-01-19 15:11:01 +0100

    feat(frame_extender-uvm): use default coverage

--- comp/mfb_tools/edit/frame_extender/uvm/tbench/env/env.sv
+++ comp/mfb_tools/edit/frame_extender/uvm/tbench/env/env.sv
@@ -17,10 +17,6 @@ class env #(int unsigned MFB_REGIONS, int unsigned MFB_REGION_SIZE, int unsigned
     uvm_logic_vector_array_mfb::env_tx #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, USERMETA_WIDTH) m_env_tx_mfb;
     uvm_logic_vector_mvb::env_tx       #(MFB_REGIONS, USERMETA_WIDTH)                                                  m_env_tx_mvb;
 
-    // Coverage models
-    uvm_mvb::coverage #(MFB_REGIONS, RX_MVB_ITEM_WIDTH) m_coverage_rx_mvb;
-    uvm_mvb::coverage #(MFB_REGIONS, USERMETA_WIDTH)    m_coverage_tx_mvb;
-
     // Scoreboard
     scoreboard #(MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, PKT_MTU, USERMETA_WIDTH, RX_MVB_ITEM_WIDTH) m_scoreboard;
     // Virtual sequencer
@@ -72,6 +68,7 @@ class env #(int unsigned MFB_REGIONS, int unsigned MFB_REGION_SIZE, int unsigned
         m_config_rx_mvb                = new;
         m_config_rx_mvb.active         = UVM_ACTIVE;
         m_config_rx_mvb.interface_name = "vif_rx_mvb";
+        m_config_rx_mvb.coverage       = 1;
         uvm_config_db #(uvm_logic_vector_mvb::config_item)::set(this, "m_env_rx_mvb", "m_config", m_config_rx_mvb);
         m_env_rx_mvb = uvm_logic_vector_mvb::env_rx #(MFB_REGIONS, RX_MVB_ITEM_WIDTH)::type_id::create("m_env_rx_mvb", this);
 
@@ -87,15 +84,9 @@ class env #(int unsigned MFB_REGIONS, int unsigned MFB_REGION_SIZE, int unsigned
         m_config_tx_mvb                = new;
         m_config_tx_mvb.active         = UVM_ACTIVE;
         m_config_tx_mvb.interface_name = "vif_tx_mvb";
+        m_config_tx_mvb.coverage       = 1;
         uvm_config_db #(uvm_logic_vector_mvb::config_item)::set(this, "m_env_tx_mvb", "m_config", m_config_tx_mvb);
         m_env_tx_mvb = uvm_logic_vector_mvb::env_tx #(MFB_REGIONS, USERMETA_WIDTH)::type_id::create("m_env_tx_mvb", this);
-
-        // ----------------------- //
-        // Coverage model creation //
-        // ----------------------- //
-
-        m_coverage_rx_mvb = new("m_coverage_rx_mvb");
-        m_coverage_tx_mvb = new("m_coverage_tx_mvb");
 
         m_scoreboard        = scoreboard        #(MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, PKT_MTU, USERMETA_WIDTH, RX_MVB_ITEM_WIDTH)                     ::type_id::create("m_scoreboard", this);
         m_virtual_sequencer = virtual_sequencer #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, USERMETA_WIDTH, RX_MVB_ITEM_WIDTH)::type_id::create("m_virtual_sequencer", this);
@@ -127,13 +118,6 @@ class env #(int unsigned MFB_REGIONS, int unsigned MFB_REGION_SIZE, int unsigned
         m_env_tx_mfb.analysis_port_meta.connect(m_scoreboard.analysis_export_tx_mfb_meta);
         // TX MVB -> Scoreboard
         m_env_tx_mvb.analysis_port.connect(m_scoreboard.analysis_export_tx_mvb);
-
-        // ------------------------- //
-        // Coverage model connection //
-        // ------------------------- //
-
-        m_env_rx_mvb.m_mvb_agent.analysis_port.connect(m_coverage_rx_mvb.analysis_export);
-        m_env_tx_mvb.m_mvb_agent.analysis_port.connect(m_coverage_tx_mvb.analysis_export);
 
         // ---------------------------- //
         // Virtual sequencer connection //
