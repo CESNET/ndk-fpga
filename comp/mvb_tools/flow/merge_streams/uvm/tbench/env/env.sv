2befc09ade46f7a934194d795348ebc419adfebe
Radek Isa <isa@cesnet.cz>
2026-01-20 08:55:01 +0100

    feat(merge_streams-uvm): use common mvb coverage

--- comp/mvb_tools/flow/merge_streams/uvm/tbench/env/env.sv
+++ comp/mvb_tools/flow/merge_streams/uvm/tbench/env/env.sv
@@ -18,8 +18,6 @@ class env #(int unsigned MVB_ITEMS, int unsigned MVB_ITEM_WIDTH, int unsigned RX
     // Coverage models
     hl_coverage_model #(MVB_ITEM_WIDTH, RX_STREAMS)            m_hl_coverage_model;
     ll_coverage_model #(MVB_ITEMS, MVB_ITEM_WIDTH, RX_STREAMS) m_ll_coverage_model;
-    uvm_mvb::coverage #(MVB_ITEMS, MVB_ITEM_WIDTH)             m_coverage_rx_mvb[RX_STREAMS];
-    uvm_mvb::coverage #(MVB_ITEMS, MVB_ITEM_WIDTH)             m_coverage_tx_mvb;
 
     // Scoreboard
     scoreboard #(MVB_ITEM_WIDTH, RX_STREAMS) m_scoreboard;
@@ -54,6 +52,7 @@ class env #(int unsigned MVB_ITEMS, int unsigned MVB_ITEM_WIDTH, int unsigned RX
             m_config_rx_mvb[i]                = new;
             m_config_rx_mvb[i].active         = UVM_ACTIVE;
             m_config_rx_mvb[i].interface_name = $sformatf("vif_rx_mvb_%0d", i);
+            m_config_rx_mvb[i].coverage = 1;
             uvm_config_db #(uvm_logic_vector_mvb::config_item)::set(this, $sformatf("m_env_rx_mvb_%0d", i), "m_config", m_config_rx_mvb[i]);
             m_env_rx_mvb[i] = uvm_logic_vector_mvb::env_rx #(MVB_ITEMS, MVB_ITEM_WIDTH)::type_id::create($sformatf("m_env_rx_mvb_%0d", i), this);
         end
@@ -62,6 +61,7 @@ class env #(int unsigned MVB_ITEMS, int unsigned MVB_ITEM_WIDTH, int unsigned RX
         m_config_tx_mvb                = new;
         m_config_tx_mvb.active         = UVM_ACTIVE;
         m_config_tx_mvb.interface_name = "vif_tx_mvb";
+        m_config_tx_mvb.coverage       = 1;
         uvm_config_db #(uvm_logic_vector_mvb::config_item)::set(this, "m_env_tx_mvb", "m_config", m_config_tx_mvb);
         m_env_tx_mvb = uvm_logic_vector_mvb::env_tx #(MVB_ITEMS, MVB_ITEM_WIDTH)::type_id::create("m_env_tx_mvb", this);
 
@@ -71,10 +71,6 @@ class env #(int unsigned MVB_ITEMS, int unsigned MVB_ITEM_WIDTH, int unsigned RX
 
         m_hl_coverage_model = hl_coverage_model #(MVB_ITEM_WIDTH, RX_STREAMS)           ::type_id::create("m_hl_coverage_model", this);
         m_ll_coverage_model = ll_coverage_model #(MVB_ITEMS, MVB_ITEM_WIDTH, RX_STREAMS)::type_id::create("m_ll_coverage_model", this);
-        for (int unsigned i = 0; i < RX_STREAMS; i++) begin
-            m_coverage_rx_mvb[i] = new($sformatf("m_coverage_rx_mvb_%0d", i));
-        end
-        m_coverage_tx_mvb = new("m_coverage_tx_mvb");
 
         m_scoreboard        = scoreboard        #(MVB_ITEM_WIDTH, RX_STREAMS)           ::type_id::create("m_scoreboard", this);
         m_virtual_sequencer = virtual_sequencer #(MVB_ITEMS, MVB_ITEM_WIDTH, RX_STREAMS)::type_id::create("m_virtual_sequencer", this);
@@ -109,10 +105,6 @@ class env #(int unsigned MVB_ITEMS, int unsigned MVB_ITEM_WIDTH, int unsigned RX
             m_env_rx_mvb[i].analysis_port.connect(m_hl_coverage_model.analysis_export);
             m_env_rx_mvb[i].m_mvb_agent.analysis_port.connect(m_ll_coverage_model.in[i].analysis_export);
         end
-        for (int unsigned i = 0; i < RX_STREAMS; i++) begin
-            m_env_rx_mvb[i].m_mvb_agent.analysis_port.connect(m_coverage_rx_mvb[i].analysis_export);
-        end
-        m_env_tx_mvb.m_mvb_agent.analysis_port.connect(m_coverage_tx_mvb.analysis_export);
 
         // ---------------------------- //
         // Virtual sequencer connection //
