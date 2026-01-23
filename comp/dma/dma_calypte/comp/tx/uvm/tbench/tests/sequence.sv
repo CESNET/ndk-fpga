709ec6c0e772e4dd773e0206ee58060df67c8378
Radek Isa <isa@cesnet.cz>
2026-01-23 13:28:31 +0100

    feat(dma_calypte_tx-uvm): remove tx sequence

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/sequence.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/sequence.sv
@@ -36,42 +36,21 @@ class virt_seq #(
     ))
 
     `uvm_declare_p_sequencer(uvm_tx_dma_calypte::sequencer #(
-        USR_MFB_REGIONS,
-        USR_MFB_REGION_SIZE,
-        USR_MFB_BLOCK_SIZE,
-        USR_MFB_ITEM_WIDTH,
         CHANNELS,
-        HDR_META_WIDTH,
-        PKT_SIZE_MAX,
-        DATA_POINTER_WIDTH,
-        PCIE_RQ_REGIONS,
-        PCIE_RQ_REGION_SIZE,
-        PCIE_RQ_BLOCK_SIZE,
-        PCIE_RQ_ITEM_WIDTH
+        DATA_POINTER_WIDTH
     ))
 
     localparam USR_MFB_META_WIDTH = HDR_META_WIDTH + $clog2(PKT_SIZE_MAX+1) + $clog2(CHANNELS);
 
-    uvm_reset::sequence_start                                                        m_reset_seq;
-    uvm_tx_dma_calypte::sequence_simple #(DATA_POINTER_WIDTH)                        m_channel_seq [CHANNELS];
-    uvm_sequence #(uvm_mfb::sequence_item #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE,
-                                            USR_MFB_ITEM_WIDTH, USR_MFB_META_WIDTH)) m_usr_mfb_seq;
-    uvm_sequence #(uvm_mfb::sequence_item #(PCIE_RQ_REGIONS, PCIE_RQ_REGION_SIZE, PCIE_RQ_BLOCK_SIZE,
-                                            PCIE_RQ_ITEM_WIDTH, sv_pcie_meta_pack::PCIE_RQ_META_WIDTH)
-                   )  m_ptr_upd_mfb_seq;
-
-    local logic [CHANNELS-1:0] m_done;
+    uvm_reset::sequence_start                                 m_reset_seq;
+    uvm_tx_dma_calypte::sequence_simple #(DATA_POINTER_WIDTH) m_channel_seq [CHANNELS];
+    protected logic [CHANNELS-1:0] m_done;
 
     function new (string name = "virt_seq");
         super.new(name);
     endfunction
 
     virtual function void init();
-        uvm_mfb::sequence_lib_tx #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH,
-                                   USR_MFB_META_WIDTH)                    m_usr_mfb_seq_lib;
-        uvm_mfb::sequence_lib_tx #(PCIE_RQ_REGIONS, PCIE_RQ_REGION_SIZE, PCIE_RQ_BLOCK_SIZE, PCIE_RQ_ITEM_WIDTH,
-                                   sv_pcie_meta_pack::PCIE_RQ_META_WIDTH) m_ptr_upd_mfb_seq_lib;
-
         m_reset_seq = uvm_reset::sequence_start::type_id::create("m_reset_seq");
 
         for (int unsigned it = 0; it < CHANNELS; it++) begin
@@ -80,37 +59,7 @@ class virt_seq #(
             m_channel_seq[it].m_packet_size_min = 1;
             m_channel_seq[it].m_packet_size_max = PKT_SIZE_MAX;
         end
-
-        m_usr_mfb_seq_lib = uvm_mfb::sequence_lib_tx #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE,
-                                                       USR_MFB_ITEM_WIDTH, USR_MFB_META_WIDTH)::type_id
-                            ::create("m_usr_mfb_seq_lib");
-        m_ptr_upd_mfb_seq_lib = uvm_mfb::sequence_lib_tx #(PCIE_RQ_REGIONS, PCIE_RQ_REGION_SIZE,
-                                                           PCIE_RQ_BLOCK_SIZE, PCIE_RQ_ITEM_WIDTH,
-                                                           sv_pcie_meta_pack::PCIE_RQ_META_WIDTH)::type_id
-                                ::create("m_ptr_upd_mfb_seq_lib");
-
-        m_usr_mfb_seq_lib.init_sequence();
-        m_ptr_upd_mfb_seq_lib.init_sequence();
-        m_usr_mfb_seq     = m_usr_mfb_seq_lib;   // NOTE: WHY????!
-        m_ptr_upd_mfb_seq = m_ptr_upd_mfb_seq_lib;
     endfunction
-
-    // This sequence needs to start over and over since it can stop the DST_RDY signal in 0 an therefore block the DUT.
-    // This happens when the m_usr_mfb_seq is shorter than the longest m_channel_seq. The same constant triggering
-    // applies for the m_ptr_upd_mfb_seq.
-    virtual task run_usr_mfb();
-        forever begin
-            assert(m_usr_mfb_seq.randomize());
-            m_usr_mfb_seq.start(p_sequencer.m_usr_mfb_sqcr);
-        end
-    endtask
-
-    virtual task run_ptr_upd_mfb();
-        forever begin
-            assert(m_ptr_upd_mfb_seq.randomize());
-            m_ptr_upd_mfb_seq.start(p_sequencer.m_ptr_upd_mfb_sqcr);
-        end
-    endtask
 
     virtual task run_reset();
         assert(m_reset_seq.randomize());
@@ -152,12 +101,6 @@ class virt_seq #(
         join_none
 
         #(200ns);
-
-        fork
-            run_usr_mfb();
-            run_ptr_upd_mfb();
-        join_none
-
         wait((& m_done) == 1);
     endtask
 endclass
--- comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/speed.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/speed.sv
@@ -5,65 +5,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 
-class virt_seq_full_speed #(
-    int unsigned USR_MFB_REGIONS,
-    int unsigned USR_MFB_REGION_SIZE,
-    int unsigned USR_MFB_BLOCK_SIZE,
-    int unsigned USR_MFB_ITEM_WIDTH,
-    int unsigned CHANNELS,
-    int unsigned HDR_META_WIDTH,
-    int unsigned PKT_SIZE_MAX,
-    int unsigned DATA_POINTER_WIDTH,
-    int unsigned PCIE_RQ_REGIONS,
-    int unsigned PCIE_RQ_REGION_SIZE,
-    int unsigned PCIE_RQ_BLOCK_SIZE,
-    int unsigned PCIE_RQ_ITEM_WIDTH
-) extends virt_seq #(
-    USR_MFB_REGIONS,
-    USR_MFB_REGION_SIZE,
-    USR_MFB_BLOCK_SIZE,
-    USR_MFB_ITEM_WIDTH,
-    CHANNELS,
-    HDR_META_WIDTH,
-    PKT_SIZE_MAX,
-    DATA_POINTER_WIDTH,
-    PCIE_RQ_REGIONS,
-    PCIE_RQ_REGION_SIZE,
-    PCIE_RQ_BLOCK_SIZE,
-    PCIE_RQ_ITEM_WIDTH
-);
-
-    `uvm_object_param_utils(test::virt_seq_full_speed #(
-        USR_MFB_REGIONS,
-        USR_MFB_REGION_SIZE,
-        USR_MFB_BLOCK_SIZE,
-        USR_MFB_ITEM_WIDTH,
-        CHANNELS,
-        HDR_META_WIDTH,
-        PKT_SIZE_MAX,
-        DATA_POINTER_WIDTH,
-        PCIE_RQ_REGIONS,
-        PCIE_RQ_REGION_SIZE,
-        PCIE_RQ_BLOCK_SIZE,
-        PCIE_RQ_ITEM_WIDTH)
-    )
-
-    function new (string name = "virt_seq_full_speed");
-        super.new(name);
-    endfunction
-
-    virtual function void init();
-        super.init();
-        m_usr_mfb_seq = uvm_mfb::sequence_full_speed_tx #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE,
-                                                          USR_MFB_ITEM_WIDTH, USR_MFB_META_WIDTH)::type_id
-                        ::create("m_usr_mfb_seq");
-        m_ptr_upd_mfb_seq = uvm_mfb::sequence_full_speed_tx #(PCIE_RQ_REGIONS, PCIE_RQ_REGION_SIZE,
-                                                              PCIE_RQ_BLOCK_SIZE, PCIE_RQ_ITEM_WIDTH,
-                                                              sv_pcie_meta_pack::PCIE_RQ_META_WIDTH)::type_id
-                            ::create("m_ptr_upd_mfb_seq");
-    endfunction
-endclass
-
 class speed extends base;
     typedef uvm_component_registry#(test::speed, "test::speed") type_id;
 
@@ -96,64 +37,42 @@ class speed extends base;
             sv_pcie_meta_pack::PCIE_CQ_META_WIDTH
         )::get_type(), "m_env.m_cq_mfb_env.*", this);
 
+
+        uvm_mfb::sequence_lib_tx #(
+                USR_MFB_REGIONS,
+                USR_MFB_REGION_SIZE,
+                USR_MFB_BLOCK_SIZE,
+                USR_MFB_ITEM_WIDTH,
+                USR_MFB_META_WIDTH
+        )::type_id::set_inst_override(
+            uvm_mfb::sequence_lib_tx_speed #(
+                USR_MFB_REGIONS,
+                USR_MFB_REGION_SIZE,
+                USR_MFB_BLOCK_SIZE,
+                USR_MFB_ITEM_WIDTH,
+                USR_MFB_META_WIDTH
+            )::get_type(), "m_env.m_tx_mfb_env.*", this
+        );
+
+        uvm_mfb::sequence_lib_tx #(
+                PCIE_CQ_MFB_REGIONS,
+                PCIE_CQ_MFB_REGION_SIZE,
+                PCIE_CQ_MFB_BLOCK_SIZE,
+                PCIE_CQ_MFB_ITEM_WIDTH,
+                sv_pcie_meta_pack::PCIE_RQ_META_WIDTH
+            )::type_id::set_inst_override(
+                uvm_mfb::sequence_lib_tx_speed #(
+                    PCIE_CQ_MFB_REGIONS,
+                    PCIE_CQ_MFB_REGION_SIZE,
+                    PCIE_CQ_MFB_BLOCK_SIZE,
+                    PCIE_CQ_MFB_ITEM_WIDTH,
+                    sv_pcie_meta_pack::PCIE_RQ_META_WIDTH
+                )::get_type(), "m_env.m_ptr_upd_mfb_env.*", this
+        );
+
+
         super.build_phase(phase);
     endfunction
-
-    // ------------------------------------------------------------------------
-    // Create environment and Run sequences o their sequencers
-    virtual task run_phase(uvm_phase phase);
-        uvm_reg_data_t pkt_cnt          [CHANNELS];
-        uvm_reg_data_t byte_cnt         [CHANNELS];
-        uvm_reg_data_t discard_pkt_cnt  [CHANNELS];
-        uvm_reg_data_t discard_byte_cnt [CHANNELS];
-        uvm_status_e   status_r;
-        time end_time;
-        virt_seq_full_speed #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH, CHANNELS,
-                              HDR_META_WIDTH, PKT_SIZE_MAX, DATA_POINTER_WIDTH, PCIE_CQ_MFB_REGIONS,
-                              PCIE_CQ_MFB_REGION_SIZE, PCIE_CQ_MFB_BLOCK_SIZE, PCIE_CQ_MFB_ITEM_WIDTH) m_virt_seq;
-
-        m_virt_seq = virt_seq_full_speed #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH,
-                                           CHANNELS, HDR_META_WIDTH, PKT_SIZE_MAX, DATA_POINTER_WIDTH,
-                                           PCIE_CQ_MFB_REGIONS, PCIE_CQ_MFB_REGION_SIZE, PCIE_CQ_MFB_BLOCK_SIZE,
-                                           PCIE_CQ_MFB_ITEM_WIDTH)::type_id::create("m_virt_seq");
-
-        phase.raise_objection(this);
-
-        m_virt_seq.init();
-        m_virt_seq.randomize();
-        m_virt_seq.start(m_env.m_sequencer);
-
-        end_time = $time();
-        `uvm_info(this.get_full_name(), $sformatf("\n\tVirtual sequence finished (%0d ns). Scoreboard used: %0d",
-                                                  end_time/1ns, m_env.m_scoreboard.used()), UVM_HIGH);
-
-        while((end_time + 200us) > $time() && (m_env.m_scoreboard.used() != 0)) begin
-            #(600ns);
-        end
-
-        for (int unsigned chan = 0; chan < CHANNELS; chan++) begin
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].sent_packets_reg.write(status_r, {32'h1, 32'h1});
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].sent_packets_reg.read(status_r, pkt_cnt[chan]);
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].sent_bytes_reg.write(status_r, {32'h1, 32'h1});
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].sent_bytes_reg.read(status_r, byte_cnt[chan]);
-
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].discarded_packets_reg.write(status_r,
-                                                                                                 {32'h1, 32'h1});
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].discarded_packets_reg.read(status_r,
-                                                                                                discard_pkt_cnt[chan]);
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].discarded_bytes_reg.write(status_r,
-                                                                                               {32'h1, 32'h1});
-            m_env.m_regmodel_top.m_regmodel.m_regmodel_channel[chan].discarded_bytes_reg.read(status_r,
-                                                                                              discard_byte_cnt[chan]);
-
-            m_env.m_scoreboard.byte_cnt[chan]         = byte_cnt[chan];
-            m_env.m_scoreboard.pkt_cnt[chan]          = pkt_cnt[chan];
-            m_env.m_scoreboard.discard_byte_cnt[chan] = discard_byte_cnt[chan];
-            m_env.m_scoreboard.discard_pkt_cnt[chan]  = discard_pkt_cnt[chan];
-        end
-
-        phase.drop_objection(this);
-    endtask
 
     function void report_phase(uvm_phase phase);
         `uvm_info(this.get_full_name(), {"\n\tTEST : ", this.get_type_name(), " END\n"}, UVM_NONE);
