aade44ad70d2996a7ded97f56714045e784dcaab
Radek Isa <isa@cesnet.cz>
2026-02-12 09:55:31 +0100

    style(uvm-lva-mvb): shorter line with class decladation

--- comp/uvm/logic_vector_array_mfb/monitor.sv
+++ comp/uvm/logic_vector_array_mfb/monitor.sv
@@ -4,8 +4,16 @@
 
 //-- SPDX-License-Identifier: BSD-3-Clause
 
-class monitor_logic_vector_array #(int unsigned REGIONS, int unsigned REGION_SIZE, int unsigned BLOCK_SIZE, int unsigned ITEM_WIDTH, int unsigned META_WIDTH) extends uvm_logic_vector_array::monitor #(ITEM_WIDTH);
-    `uvm_component_param_utils(uvm_logic_vector_array_mfb::monitor_logic_vector_array #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH))
+class monitor_logic_vector_array #(
+    int unsigned REGIONS,
+    int unsigned REGION_SIZE,
+    int unsigned BLOCK_SIZE,
+    int unsigned ITEM_WIDTH,
+    int unsigned META_WIDTH
+) extends uvm_logic_vector_array::monitor #(ITEM_WIDTH);
+    `uvm_component_param_utils(
+        uvm_logic_vector_array_mfb::monitor_logic_vector_array #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)
+    )
 
     // Analysis port
     typedef monitor_logic_vector_array #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH) this_type;
@@ -113,8 +121,16 @@ class monitor_logic_vector_array #(int unsigned REGIONS, int unsigned REGION_SIZ
     endfunction
 endclass
 
-class monitor_logic_vector #(int unsigned REGIONS, int unsigned REGION_SIZE, int unsigned BLOCK_SIZE, int unsigned ITEM_WIDTH, int unsigned META_WIDTH) extends uvm_logic_vector::monitor#(META_WIDTH);
-    `uvm_component_param_utils(uvm_logic_vector_array_mfb::monitor_logic_vector #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH))
+class monitor_logic_vector #(
+    int unsigned REGIONS,
+    int unsigned REGION_SIZE,
+    int unsigned BLOCK_SIZE,
+    int unsigned ITEM_WIDTH,
+    int unsigned META_WIDTH
+) extends uvm_logic_vector::monitor#(META_WIDTH);
+    `uvm_component_param_utils(
+        uvm_logic_vector_array_mfb::monitor_logic_vector #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, META_WIDTH)
+    )
 
     //localparam ITEM_WIDTH = 32;
 
