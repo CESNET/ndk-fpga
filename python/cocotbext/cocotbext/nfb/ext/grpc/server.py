10491b4524fc356c3d7135495bc41a3bbe2e7c67
Martin Spinler <spinler@cesnet.cz>
2026-02-19 08:05:38 +0100

    feat(cocotbext): enable custom servicers in NfbDmaThreadedGrpcServer

--- python/cocotbext/cocotbext/nfb/ext/grpc/server.py
+++ python/cocotbext/cocotbext/nfb/ext/grpc/server.py
@@ -26,13 +26,14 @@ class NfbDmaThreadedGrpcServer:
 
         self._port = port
 
-        self._mi_reciver = NfbServicer(dev)
-        self._dma_reciver = DmaServicer(ram)
+        self._nfb_reciver = dev if isinstance(dev, nfb_pb_grpc.NfbServicer) else NfbServicer(dev)
+        self._dma_reciver = ram if isinstance(ram, dma_pb_grpc.DmaServicer) or ram is None else DmaServicer(ram)
 
         self._server = grpc.server(futures.ThreadPoolExecutor())
         self._server.add_insecure_port(f"{addr}:{port}")
-        nfb_pb_grpc.add_NfbServicer_to_server(self._mi_reciver, self._server)
-        dma_pb_grpc.add_DmaServicer_to_server(self._dma_reciver, self._server)
+        nfb_pb_grpc.add_NfbServicer_to_server(self._nfb_reciver, self._server)
+        if self._dma_reciver:
+            dma_pb_grpc.add_DmaServicer_to_server(self._dma_reciver, self._server)
 
         self._thread = Thread(target=self._run)
         self._thread_terminate = False
@@ -40,17 +41,25 @@ class NfbDmaThreadedGrpcServer:
     def _run(self):
         self._server.start()
 
-        while not cocotb.regression_manager._tearing_down and not self._thread_terminate:
-            time.sleep(0.1)
+        if cocotb.regression_manager is not None:
+            while not cocotb.regression_manager._tearing_down and not self._thread_terminate:
+                time.sleep(0.1)
 
-        self._dma_reciver._logout()
-        self._mi_reciver.resp_force()
+        if isinstance(self._dma_reciver, DmaServicer):
+            self._dma_reciver._logout()
+        if isinstance(self._nfb_reciver, NfbServicer):
+            self._nfb_reciver.resp_force()
         self._server.stop(2.0)
         self._server.wait_for_termination()
 
+    def path(self):
+        addr = socket.gethostname()
+        dma = "+dma_vas" if self._dma_reciver else ""
+        return f"libnfb-ext-grpc.so:grpc{dma}:{addr}:{self._port}"
+
     def start(self):
         self._thread.start()
-        self._log.info(f"gRPC server started, listening on {self._port}. Device string: libnfb-ext-grpc.so:grpc+dma_vas:{socket.gethostname()}:{self._port}")
+        self._log.info(f"gRPC server started, listening on {self._port}. Device string: {self.path()}")
 
     def close(self):
         self._thread_terminate = True
