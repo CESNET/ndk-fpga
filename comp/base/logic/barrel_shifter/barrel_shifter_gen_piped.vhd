e94701dda2749d00ee1460bbd6b051b704e8ef4d
Radek Isa <isa@cesnet.cz>
2026-03-05 22:10:40 +0100

    perf(barrel-shifter-gen-piped): optimize timing and resource usage
        `rotate_left` and `rotate_right` now use significantly fewer resources and
        Improved timing performance

--- comp/base/logic/barrel_shifter/barrel_shifter_gen_piped.vhd
+++ comp/base/logic/barrel_shifter/barrel_shifter_gen_piped.vhd
@@ -1,6 +1,6 @@
 -- barrel_shifter_gen.vhd: Barrel shifter with generic data width, generic block size and optional pipelining and output register
--- Copyright (C) 2019 CESNET
--- Author(s): Jan Kubalek <xkubal11@stud.fit.vutbr.cz>
+-- Copyright (C) 2026 CESNET
+-- Author(s): Radek Iša <isa@cesnet.cz>
 --
 -- SPDX-License-Identifier: BSD-3-Clause
 --
@@ -8,6 +8,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
 use work.math_pack.all;
 use work.type_pack.all;
 
@@ -59,72 +60,169 @@ end entity;
 -- ----------------------------------------------------------------------------
 
 architecture FULL of BARREL_SHIFTER_GEN_PIPED is
+    constant DATA_WIDTH : natural := BLOCKS*BLOCK_WIDTH;
 
-    function getmuxmetadata (metadata : std_logic_vector; index : integer) return std_logic_vector is
-    begin
-        if (index = 0) then
-            return metadata;
-        else
-            return (metadata'high downto 0 => '0');
-        end if;
-    end function;
+    -- INPUT STAGE
+    signal r0_data     : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal r0_sel      : std_logic_vector(log2(BLOCKS)-1 downto 0);
+    signal r0_metadata : std_logic_vector(METADATA_WIDTH-1 downto 0);
+    signal r0_src_rdy  : std_logic;
+    signal r0_dst_rdy  : std_logic;
 
-    signal data_in_deser   : slv_array_t(BLOCKS-1 downto 0)(BLOCK_WIDTH-1 downto 0);
-    signal mux_in          : slv_array_2d_t(BLOCKS-1 downto 0)(BLOCKS-1 downto 0)(BLOCK_WIDTH-1 downto 0);
-    signal mux_in_dst_rdy  : std_logic_vector(BLOCKS-1 downto 0);
-    signal mux_out         : slv_array_t(BLOCKS-1 downto 0)(BLOCK_WIDTH-1 downto 0);
-    signal mux_meta_out    : slv_array_t(BLOCKS-1 downto 0)(METADATA_WIDTH-1 downto 0);
-    signal mux_out_src_rdy : std_logic_vector(BLOCKS-1 downto 0);
+    signal r1_data_in  : unsigned(2*DATA_WIDTH-1 downto 0);
+    signal r1_data_rot : unsigned(2*DATA_WIDTH-1 downto 0);
+    signal r1_data     : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal r1_sel      : integer range 0 to maximum(BLOCKS,0)-1; -- Don't change it. It would requires more resources.
+    signal r1_metadata : std_logic_vector(METADATA_WIDTH-1 downto 0);
+    signal r1_src_rdy  : std_logic;
+    signal r1_dst_rdy  : std_logic;
+
+    signal r2_data     : slv_array_t(BAR_SHIFT_LATENCY downto 0)(DATA_WIDTH-1 downto 0);
+    signal r2_metadata : slv_array_t(BAR_SHIFT_LATENCY downto 0)(METADATA_WIDTH-1 downto 0);
+    signal r2_src_rdy  : std_logic_vector(BAR_SHIFT_LATENCY downto 0);
+    signal r2_dst_rdy  : std_logic_vector(BAR_SHIFT_LATENCY downto 0);
+
+    signal r3_data     : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal r3_metadata : std_logic_vector(METADATA_WIDTH-1 downto 0);
+    signal r3_src_rdy  : std_logic;
+    signal r3_dst_rdy  : std_logic;
+
 begin
 
-    data_in_deser <= slv_array_downto_deser(RX_DATA, BLOCKS, BLOCK_WIDTH);
-    RX_DST_RDY    <= mux_in_dst_rdy(0);
+    -------------------------------------------------
+    -- R0 STAGE
+    -------------------------------------------------
+    input_reg_gen : if (INPUT_REG) generate
+        signal  rdy : std_logic;
+    begin
 
-    mux_leftg: if SHIFT_LEFT = true generate
-        mux_leftgg: for i in 0 to BLOCKS-1 generate
-            mux_leftggg: for j in 0 to BLOCKS-1 generate
-                mux_in(i)(j) <= data_in_deser((i-j) mod BLOCKS);
-            end generate;
-        end generate;
+        -- set  DST_RDY when previous pipeline is ready or this
+        -- stage is empty
+        rdy        <= '1' when r0_src_rdy = '0' or r0_dst_rdy = '1' else '0';
+        RX_DST_RDY <= rdy;
+
+        process (CLK)
+        begin
+            if rising_edge(CLK) then
+                if (RESET = '1') then
+                    r0_src_rdy  <= '0';
+                elsif (rdy = '1') then
+                    r0_src_rdy  <= RX_SRC_RDY;
+                end if;
+
+                if (rdy = '1') then
+                    r0_data     <= RX_DATA;
+                    r0_sel      <= RX_SEL;
+                    r0_metadata <= RX_METADATA;
+                end if;
+            end if;
+        end process;
+    else generate
+        r0_data     <= RX_DATA;
+        r0_sel      <= RX_SEL;
+        r0_metadata <= RX_METADATA;
+        r0_src_rdy  <= RX_SRC_RDY;
+        RX_DST_RDY  <= r0_dst_rdy;
     end generate;
 
-    mux_rightg: if SHIFT_LEFT = false generate
-        mux_rightgg: for i in 0 to BLOCKS-1 generate
-            mux_rightggg: for j in 0 to BLOCKS-1 generate
-                mux_in(i)(j) <= data_in_deser((j+i) mod BLOCKS);
-            end generate;
-        end generate;
+    -------------------------------------------------
+    -- R1 STAGE - ROTATION
+    -------------------------------------------------
+    r1_metadata <= r0_metadata;
+    r1_data_in  <= unsigned(r0_data) & unsigned(r0_data);
+    r1_src_rdy  <= r0_src_rdy;
+    r0_dst_rdy  <= r1_dst_rdy;
+
+    sel_int_gen : if (BLOCKS > 1) generate
+        r1_sel <= to_integer(unsigned(r0_sel)) when unsigned(r0_sel) < BLOCKS else
+                  to_integer(unsigned'(log2(BLOCKS)-1 downto 0 => 'X'));
+    else generate
+        r1_sel <= 0;
     end generate;
 
-    muxg: for i in 0 to BLOCKS-1 generate
-        muxi: entity work.GEN_MUX_PIPED
-        generic map (
-            DATA_WIDTH     => BLOCK_WIDTH,
-            MUX_WIDTH      => BLOCKS,
-            MUX_LATENCY    => BAR_SHIFT_LATENCY,
-            INPUT_REG      => INPUT_REG,
-            OUTPUT_REG     => OUTPUT_REG,
-            METADATA_WIDTH => METADATA_WIDTH
-        )
-        port map (
-            CLK   => CLK,
-            RESET => RESET,
-
-            RX_DATA     => slv_array_ser(mux_in(i), BLOCKS, BLOCK_WIDTH),
-            RX_SEL      => RX_SEL,
-            RX_METADATA => getMuxMetadata(RX_METADATA,i), -- only propagate the real metadata through the firts multiplexer
-            RX_SRC_RDY  => RX_SRC_RDY,
-            RX_DST_RDY  => mux_in_dst_rdy(i),
-
-            TX_DATA     => mux_out(i),
-            TX_METADATA => mux_meta_out(i),
-            TX_SRC_RDY  => mux_out_src_rdy(i),
-            TX_DST_RDY  => TX_DST_RDY
-        );
+    shift_sel_gen : if (SHIFT_LEFT) generate
+        r1_data_rot <= IEEE.numeric_std.shift_left(r1_data_in, r1_sel*BLOCK_WIDTH);
+        r1_data     <= std_logic_vector(r1_data_rot(DATA_WIDTH*2-1 downto DATA_WIDTH));
+    else generate
+        r1_data_rot <= IEEE.numeric_std.shift_right(r1_data_in, r1_sel*BLOCK_WIDTH);
+        r1_data     <= std_logic_vector(r1_data_rot(DATA_WIDTH-1 downto 0));
     end generate;
 
-    TX_DATA     <= slv_array_ser(mux_out, BLOCKS, BLOCK_WIDTH);
-    TX_METADATA <= mux_meta_out(0);
-    TX_SRC_RDY  <= mux_out_src_rdy(0);
+
+    -------------------------------------------------
+    -- R2 PIPELINE
+    -------------------------------------------------
+    r2_data(0)     <= r1_data;
+    r2_metadata(0) <= r1_metadata;
+    r2_src_rdy(0)  <= r1_src_rdy;
+    r1_dst_rdy     <= r2_dst_rdy(0);
+
+    pipeline_gen : for it in 0 to BAR_SHIFT_LATENCY-1 generate
+        signal  rdy : std_logic;
+    begin
+
+        -- set  DST_RDY when previous pipeline is ready or this
+        -- stage is empty
+        rdy            <= '1' when r2_src_rdy(it+1) = '0' or r2_dst_rdy(it+1) = '1' else '0';
+        r2_dst_rdy(it) <= rdy;
+
+        process (CLK)
+        begin
+            if rising_edge(CLK) then
+                if (RESET = '1') then
+                    r2_src_rdy(it+1)  <= '0';
+                elsif (rdy = '1') then
+                    r2_src_rdy(it+1) <= r2_src_rdy(it);
+                end if;
+
+                if (rdy = '1') then
+                    r2_data(it+1)     <= r2_data(it);
+                    r2_metadata(it+1) <= r2_metadata(it);
+                end if;
+            end if;
+        end process;
+    end generate;
+
+    -------------------------------------------------
+    -- R3 OUTPUT REG/ OUTPUT
+    -------------------------------------------------
+    output_reg_gen : if (OUTPUT_REG) generate
+        signal  rdy : std_logic;
+    begin
+
+        -- set  DST_RDY when previous pipeline is ready or this
+        -- stage is empty
+        rdy                           <= '1' when r3_src_rdy = '0' or r3_dst_rdy = '1' else '0';
+        r2_dst_rdy(BAR_SHIFT_LATENCY) <= rdy;
+
+        process (CLK)
+        begin
+            if rising_edge(CLK) then
+                if (RESET = '1') then
+                    r3_src_rdy  <= '0';
+                elsif (rdy = '1') then
+                    r3_src_rdy  <= r2_src_rdy(BAR_SHIFT_LATENCY);
+                end if;
+
+                if (rdy = '1') then
+                    r3_data      <= r2_data(BAR_SHIFT_LATENCY);
+                    r3_metadata  <= r2_metadata(BAR_SHIFT_LATENCY);
+                end if;
+            end if;
+        end process;
+    else generate
+        r3_data                        <= r2_data(BAR_SHIFT_LATENCY);
+        r3_metadata                    <= r2_metadata(BAR_SHIFT_LATENCY);
+        r3_src_rdy                     <= r2_src_rdy(BAR_SHIFT_LATENCY);
+        r2_dst_rdy(BAR_SHIFT_LATENCY)  <= r3_dst_rdy;
+    end generate;
+
+    -------------------------------------------------
+    -- SET OUTPUT
+    -------------------------------------------------
+    TX_DATA     <= r3_data;
+    TX_METADATA <= r3_metadata;
+    TX_SRC_RDY  <= r3_src_rdy;
+    r3_dst_rdy  <= TX_DST_RDY;
 
 end architecture;
