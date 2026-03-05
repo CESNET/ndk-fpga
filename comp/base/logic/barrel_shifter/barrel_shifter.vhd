bc98f13fc1d72ac671037be95e8b987b6961255c
Radek Isa <isa@cesnet.cz>
2026-03-05 14:10:58 +0100

    perf(barrel-shifter): use implementation from barrel-shifter-gen

--- comp/base/logic/barrel_shifter/barrel_shifter.vhd
+++ comp/base/logic/barrel_shifter/barrel_shifter.vhd
@@ -1,26 +1,20 @@
---
 -- barrel_shifter.vhd Barrel shifter with generic data width
--- Copyright (C) 2009 CESNET
--- Author(s): Vaclav Bartos <washek@liberouter.org>
+-- Copyright (C) 2026 CESNET
+-- Author(s): Radek Iša <isa@cesnet.cz>
 --
 -- SPDX-License-Identifier: BSD-3-Clause
---
--- $Id$
---
--- TODO:
 --
 
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.std_logic_arith.all;
-use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
 
 use work.math_pack.all;
 
 -- ----------------------------------------------------------------------------
 --                  ENTITY DECLARATION -- Barrel shifter                     --
 -- ----------------------------------------------------------------------------
-
+-- Note: please prefr BARREL_SHIFTER_GEN instead
 entity BARREL_SHIFTER is
     generic (
         DATA_WIDTH  : integer := 64;
@@ -36,33 +30,26 @@ entity BARREL_SHIFTER is
     );
 end entity;
 
--- ----------------------------------------------------------------------------
---                       ARCHITECTURE DECLARATION                            --
--- ----------------------------------------------------------------------------
 
 architecture BARREL_SHIFTER_ARCH of BARREL_SHIFTER is
     constant BLOCK_WIDTH : natural := DATA_WIDTH / BLOCKS;
 begin
 
-    assert DATA_WIDTH mod BLOCKS = 0
-        report "DATA_WIDTH is not multiple of BLOCKS"
-        severity Failure;
+    assert (DATA_WIDTH mod BLOCKS = 0)
+        report "DATA_WIDTH must be divisible by BLOCK"
+        severity failure;
 
-    multiplexors: for i in 0 to BLOCKS-1 generate
-        process (DATA_IN, SEL)
-            variable sel_aux : integer;
-            variable sel_blk : integer;
-        begin
-            if (SHIFT_LEFT) then
-                sel_aux := conv_integer('0'&SEL);
-            else
-                sel_aux := conv_integer('0'&(0-SEL));
-            end if;
-
-            sel_blk := ((BLOCKS-sel_aux+i) mod BLOCKS);
-
-            DATA_OUT((i+1)*BLOCK_WIDTH-1 downto i*BLOCK_WIDTH) <= DATA_IN((sel_blk+1)*BLOCK_WIDTH-1 downto sel_blk*BLOCK_WIDTH);
-        end process;
-    end generate;
-
+    shifter_inst : entity work.BARREL_SHIFTER_GEN
+    generic map (
+        BLOCKS     => BLOCKS,
+        BLOCK_SIZE => BLOCK_WIDTH,
+        SHIFT_LEFT => SHIFT_LEFT
+    )
+    port map (
+        DATA_IN  => DATA_IN,
+        DATA_OUT => DATA_OUT,
+        SEL      => SEL
+    );
 end architecture;
+
+
