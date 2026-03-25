f99524cd2d2f1e52a737de346ed70caf12904c8c
Jakub Cabal <cabal@cesnet.cz>
2026-03-25 21:13:51 +0100

    feat(axis-tools): introduce AXIS_TAIL_TRIMMER module

---
+++ comp/axis_tools/edit/tail_trimmer/Modules.tcl
@@ -0,0 +1,10 @@
+# Modules.tcl: Components include script
+# Copyright (C) 2026 CESNET z. s. p. o.
+# Author(s): Jakub Cabal <cabal@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/math_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/type_pack.vhd"
+
+lappend MOD "$ENTITY_BASE/axis_tail_trimmer.vhd"
