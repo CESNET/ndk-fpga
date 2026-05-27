1d15a6b321883f351f2c4f2f250b0b1d6f1fd77d
Tomas Fukac <fukac@dyna-nic.com>
2026-05-27 15:56:56 +0200

    feat(mfb_cutter): add support for cut offset

--- comp/mfb_tools/flow/cutter_simple/Modules.tcl
+++ comp/mfb_tools/flow/cutter_simple/Modules.tcl
@@ -6,6 +6,7 @@
 
 # Set paths
 set BIN2HOT_BASE "$OFM_PATH/comp/base/logic/bin2hot"
+set DEC1FN2B_BASE "$OFM_PATH/comp/base/logic/dec1fn"
 
 # Packages
 set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/math_pack.vhd"
@@ -13,7 +14,8 @@ set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/type_pack.vhd"
 
 # Components
 set COMPONENTS [concat $COMPONENTS [list \
-    [ list "BIN2HOT" $BIN2HOT_BASE "FULL" ] \
+    [ list "BIN2HOT"  $BIN2HOT_BASE  "FULL" ] \
+    [ list "DEC1FN2B" $DEC1FN2B_BASE "FULL" ] \
 ]]
 
 # Source files for implemented component
