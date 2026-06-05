a81c9fede439553f66fa32e3154b9855e047aa84
Tomas Fukac <fukac@dyna-nic.com>
2026-06-05 10:26:42 +0200

    chore(mfb_cutter-uvm): add parameter and combinations for cut offset

--- comp/mfb_tools/flow/cutter_simple/ver/ver_settings.py
+++ comp/mfb_tools/flow/cutter_simple/ver/ver_settings.py
@@ -9,8 +9,9 @@ SETTINGS = {
         "BLOCK_SIZE"         : "8",
         "ITEM_WIDTH"         : "8",
         "CUTTED_ITEMS"       : "27",
+        "MAX_CUT_OFFSET"     : "64",
         "FRAME_SIZE_MAX"     : "512",
-        "FRAME_SIZE_MIN"     : "64+27",
+        "FRAME_SIZE_MIN"     : "64+CUTTED_ITEMS+MAX_CUT_OFFSET",
         "TRANSACTION_COUNT"  : "2000",
     },
     "pcie" : {
@@ -57,10 +58,22 @@ SETTINGS = {
     },
     "cutted_items_max" : {
         "CUTTED_ITEMS"       : "REGION_SIZE*BLOCK_SIZE",
-        "FRAME_SIZE_MIN"     : "64+(REGION_SIZE*BLOCK_SIZE)"
+        "FRAME_SIZE_MIN"     : "64+(REGION_SIZE*BLOCK_SIZE)+MAX_CUT_OFFSET"
     },
     "cutted_items_comb_1" : {
         "CUTTED_ITEMS"          : "1",
+    },
+    "cut_offset_0" : {
+        "MAX_CUT_OFFSET" : "0"
+    },
+    "cut_offset_1" : {
+        "MAX_CUT_OFFSET" : "1"
+    },
+    "cut_offset_small" : {
+        "MAX_CUT_OFFSET" : "2"
+    },
+    "cut_offset_large" : {
+        "MAX_CUT_OFFSET" : "128"
     },
     "_combinations_" : (
     (), # Works the same as '("default",),' as the "default" is applied in every combination
@@ -70,13 +83,28 @@ SETTINGS = {
 
     ("cutted_items_max",),
 
+    ("cut_offset_0",),
+    ("cut_offset_1",),
+    ("cut_offset_small",),
+    ("cut_offset_large",),
+
     ("pcie", "cutted_items_max",),
+    ("pcie", "cutted_items_max", "cut_offset_small",),
+    ("pcie", "cutted_items_max", "cut_offset_large",),
     ("pcie", "cutted_items_comb_1",),
+    ("pcie", "cutted_items_comb_1", "cut_offset_small",),
+    ("pcie", "cutted_items_comb_1", "cut_offset_large",),
 
     ("region_comb_1", "cutted_items_max",),
+    ("region_comb_1", "cutted_items_max", "cut_offset_small",),
+    ("region_comb_1", "cutted_items_max", "cut_offset_large",),
     ("region_comb_1", "cutted_items_comb_1",),
+    ("region_comb_1", "cutted_items_comb_1", "cut_offset_small",),
+    ("region_comb_1", "cutted_items_comb_1", "cut_offset_large",),
 
     ("region_comb_2", "cutted_items_comb_1",),
+    ("region_comb_2", "cutted_items_comb_1", "cut_offset_small",),
+    ("region_comb_2", "cutted_items_comb_1", "cut_offset_large",),
 
     ("region_comb_3", "cutted_items_max",),
     ("region_comb_3", "cutted_items_comb_1",),
