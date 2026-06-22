a371d8936bc0387549f6a6fd34b1f7d1c7fe0a70
Yaroslav Marushchenko <xmarus09@stud.fit.vutbr.cz>
2026-06-22 08:45:43 +0200

    fix(tx_mfb_gen): scale packet planner address space with region number

--- comp/mfb_tools/debug/generator/mfb_generator_core_pacp.vhd
+++ comp/mfb_tools/debug/generator/mfb_generator_core_pacp.vhd
@@ -66,7 +66,7 @@ architecture FULL of MFB_GENERATOR_CORE_PACP is
     constant REGION_ITEMS   : natural := REGION_WIDTH/ITEM_WIDTH;
     constant WORD_ITEMS     : natural := WORD_WIDTH/ITEM_WIDTH;
 
-    constant SPACE_SIZE     : natural := 2**(LENGTH_WIDTH+1);
+    constant SPACE_SIZE     : natural := 2**(LENGTH_WIDTH)*REGIONS;
 
     constant SPACE_WORDS    : natural := SPACE_SIZE/WORD_ITEMS;
 
