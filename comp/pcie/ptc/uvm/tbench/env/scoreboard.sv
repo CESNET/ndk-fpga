da70db5baa98586eb11bfd0b58af8538758f068a
Radek Isa <isa@cesnet.cz>
2026-06-23 19:26:57 +0200

    fix(ptc-uvm): stop comparing higher bits of unitid

--- comp/pcie/ptc/uvm/tbench/env/scoreboard.sv
+++ comp/pcie/ptc/uvm/tbench/env/scoreboard.sv
@@ -4,13 +4,40 @@
 
 //-- SPDX-License-Identifier: BSD-3-Clause
 
+class scoreboard_dma_rc #(DMA_PORTS) extends uvm_common::comparer_ordered   #(uvm_dma::sequence_item_rc);
+    `uvm_component_param_utils(uvm_ptc::scoreboard_dma_rc #(DMA_PORTS))
+
+    // Contructor of scoreboard.
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    // NOTE: In the newer interpretation, the unit ID is used for routing.
+    // Each level is responsible for its own higher-order bits, so the unit ID
+    // in a request does not need to match the unit ID in the response.
+    // However, the lower-order bits must still match.
+    virtual function void write_model(MODEL_ITEM tr);
+        if (DMA_PORTS > 1) begin
+            tr.unit_id[sv_dma_bus_pack::DMA_REQUEST_UNITID_W-1 -: $clog2(DMA_PORTS)] = 0;
+        end
+        super.write_model(tr);
+    endfunction
+
+    virtual function void write_dut(DUT_ITEM tr);
+        if (DMA_PORTS > 1) begin
+            tr.unit_id[sv_dma_bus_pack::DMA_REQUEST_UNITID_W-1 -: $clog2(DMA_PORTS)] = 0;
+        end
+        super.write_dut(tr);
+    endfunction
+endclass
+
 class scoreboard#(
     int unsigned DMA_PORTS
 )extends uvm_scoreboard;
     `uvm_component_param_utils(uvm_ptc::scoreboard#(DMA_PORTS))
 
     uvm_common::comparer_unordered #(uvm_pcie::header) pcie_rq_cmp;
-    uvm_common::comparer_ordered   #(uvm_dma::sequence_item_rc) dma_rc_cmp[DMA_PORTS];
+    scoreboard_dma_rc#(DMA_PORTS) dma_rc_cmp[DMA_PORTS];
 
     // Contructor of scoreboard.
     function new(string name, uvm_component parent);
@@ -39,7 +66,7 @@ class scoreboard#(
 
         pcie_rq_cmp = uvm_common::comparer_unordered #(uvm_pcie::header)::type_id::create("pcie_rq_cmp", this);
         for (int it = 0; it < DMA_PORTS; it++) begin
-            dma_rc_cmp[it] = uvm_common::comparer_ordered#(uvm_dma::sequence_item_rc)::type_id::create($sformatf("dma_rc_cmp_%0d", it), this);
+            dma_rc_cmp[it] = scoreboard_dma_rc#(DMA_PORTS)::type_id::create($sformatf("dma_rc_cmp_%0d", it), this);
         end
     endfunction
 
