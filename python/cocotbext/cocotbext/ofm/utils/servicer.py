d1b86580dfecdd6af8472bbaf189c2a4003b2831
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2026-07-10 17:19:30 +0200

    feat(cocotbext - utils): update external functions to cocotb 2.0 standard

--- python/cocotbext/cocotbext/ofm/utils/servicer.py
+++ python/cocotbext/cocotbext/ofm/utils/servicer.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (C) 2024 CESNET z. s. p. o.
+# Copyright (C) 2024-2026 CESNET z. s. p. o.
 # Author(s): Ondrej Schwarz <ondrejschwarz@cesnet.cz>
 
 import logging
-import cocotb
 
+import cocotb
 import nfb.ext.python as ext
 
 
@@ -18,17 +18,17 @@ class Servicer(ext.AbstractNfb):
         # TODO: fix mi[0]
         return (self._device, node.get_property("reg")[0])
 
-    @cocotb.function
-    def read(self, bus_node, node, offset, nbyte):
+    @cocotb.task.resume
+    async def read(self, bus_node, node, offset, nbyte):
         mi, base = self.get_node_base(bus_node, node)
-        data = yield mi.read(offset, nbyte)
+        data = await mi.read(offset, nbyte)
         self._log.debug(f"comp read: size: {nbyte:>2}, offset: {offset:04x}, path: {node.path}/{node.name}, data:", data)
         return bytes(data)
 
-    @cocotb.function
-    def write(self, bus_node, node, offset, data):
+    @cocotb.task.resume
+    async def write(self, bus_node, node, offset, data):
         mi, base = self.get_node_base(bus_node, node)
         nbyte = len(data)
         #data = list(data)
         self._log.debug(f"comp write: size: {nbyte:>2}, offset: {offset:04x}, path: {node.path}/{node.name}, data:", data)
-        yield mi.write(offset, data)
+        await mi.write(offset, data)
