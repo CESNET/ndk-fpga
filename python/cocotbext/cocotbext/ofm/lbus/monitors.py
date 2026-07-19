35bafb661aa0f0a7c706087829c9ea8499ee435a
Ondrej Schwarz <ondrejschwarz@cesnet.cz>
2026-07-19 13:44:28 +0200

    feat(cocotbext - lbus): upgrade LBus monitor to cocotb 2.0

--- python/cocotbext/cocotbext/ofm/lbus/monitors.py
+++ python/cocotbext/cocotbext/ofm/lbus/monitors.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (C) 2023 CESNET z. s. p. o.
+# Copyright (C) 2023-2026 CESNET z. s. p. o.
 # Author(s): Martin Spinler <spinler@cesnet.cz>
 
 from cocotb_bus.monitors import BusMonitor
@@ -13,7 +13,7 @@ class LBusMonitor(BusMonitor):
     def __init__(self, *args, packets=False, aux_signals=False,
                  data_type="buff", **kwargs):
 
-        BusMonitor.__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     async def _monitor_recv(self):
         clk_redge = RisingEdge(self.clock)
@@ -23,18 +23,18 @@ class LBusMonitor(BusMonitor):
             await clk_redge
 
             for i in range(self._segments):
-                if self.bus.ena.value.integer & (1 << i):
-                    if self.bus.sop.value.integer & (1 << i):
+                if self.bus.ena.value.to_unsigned() & (1 << i):
+                    if self.bus.sop.value.to_unsigned() & (1 << i):
                         assert packet is None
                         packet = []
 
-                    data = self.bus.data[i].value.buff
-                    if self.bus.eop.value.integer & (1 << i):
-                        empty_count = self.bus.mty[i].value.integer
+                    data = self.bus.data[i].value.to_bytes(byteorder="big")
+                    if self.bus.eop.value.to_unsigned() & (1 << i):
+                        empty_count = self.bus.mty[i].value.to_unsigned()
                         data = data[0:-empty_count] if empty_count else data
 
                     packet += data
 
-                    if self.bus.eop.value.integer & (1 << i):
+                    if self.bus.eop.value.to_unsigned() & (1 << i):
                         self._recv(bytes(packet))
                         packet = None
