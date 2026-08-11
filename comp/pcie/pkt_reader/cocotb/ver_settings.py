93378f73519d33dee164a058e6514a96f4a48304
Daniel Kondys <xkondy00@vutbr.cz>
2026-08-11 10:52:01 +0200

    test(pcie-pkt_reader): add ver_settings for the cocotb test

---
+++ comp/pcie/pkt_reader/cocotb/ver_settings.py
@@ -0,0 +1,71 @@
+# ver_settings.py
+# Copyright (C) 2026 CESNET z. s. p. o.
+# Author(s): Daniel Kondys <kondys@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+SETTINGS = {
+    # The default setting of verification. Only changed generics are listed.
+    "default" : {
+        "PCIE_DOWN_REGIONS"     : "2",
+        "PCIE_DOWN_REGION_SIZE" : "1",
+        "PCIE_DOWN_BLOCK_SIZE"  : "8",
+        "PCIE_DOWN_ITEM_WIDTH"  : "32",
+        "PKT_MTU"               : "2**12",
+        "RESP_IN_ORDER"         : "True",
+    },
+
+    # PCIe down MFB interface variants
+    "pcie_down_1r_1b_8i" : {
+        "PCIE_DOWN_REGIONS"     : "1",
+        "PCIE_DOWN_REGION_SIZE" : "1",
+        "PCIE_DOWN_BLOCK_SIZE"  : "8",
+        "PCIE_DOWN_ITEM_WIDTH"  : "32",
+    },
+    "pcie_down_4r_1b_4i" : {
+        "PCIE_DOWN_REGIONS"     : "4",
+        "PCIE_DOWN_REGION_SIZE" : "1",
+        "PCIE_DOWN_BLOCK_SIZE"  : "4",
+        "PCIE_DOWN_ITEM_WIDTH"  : "32",
+    },
+    "pcie_down_4r_1b_8i" : {
+        "PCIE_DOWN_REGIONS"     : "4",
+        "PCIE_DOWN_REGION_SIZE" : "1",
+        "PCIE_DOWN_BLOCK_SIZE"  : "8",
+        "PCIE_DOWN_ITEM_WIDTH"  : "32",
+    },
+
+    # Packet MTU variants
+    "mtu_256" : {
+        "PKT_MTU"               : "2**8",
+    },
+    "mtu_max" : {
+        "PKT_MTU"               : "16383",
+    },
+
+    # Output responses as they complete, ignoring the request order
+    "resp_out_of_order" : {
+        "RESP_IN_ORDER"         : "False",
+    },
+
+    "_combinations_" : (
+    (), # Works the same as '("default",),' as the "default" is applied in every combination
+
+    ("pcie_down_1r_1b_8i",),
+    ("pcie_down_4r_1b_4i",),
+    ("pcie_down_4r_1b_8i",),
+    ("mtu_256",),
+    ("mtu_max",),
+
+    ("resp_out_of_order",),
+    ("resp_out_of_order","pcie_down_4r_1b_8i"),
+    ("resp_out_of_order","mtu_256"),
+    ("resp_out_of_order","mtu_max"),
+
+    ("pcie_down_1r_1b_8i","mtu_max"),
+    ("pcie_down_4r_1b_4i","mtu_256"),
+
+    ("pcie_down_4r_1b_4i","resp_out_of_order","mtu_max"),
+
+    ),
+}
